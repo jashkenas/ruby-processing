650833be89fee4e9c2ab7030d8a8c274f7087c13
Jeremy Ashkenas <jashkenas@gmail.com>
2008-05-03 14:17:42 -0400

    Added hooks for live coding.
    Former-commit-id: 9c020ef3c875892141d2e8c6e5349771965e5585
    Former-commit-id: 7a781bdf2f4d9946bc9699445561c19ad1e33d03

--- script/live_files/empty_coding.rb
+++
@@ -1,6 +0,0 @@
-# If you don't pass in a class, it just starts up IRB with 
-# Ruby-Processing loaded.
-
-require 'irb'
-require 'ruby-processing'
-IRB.start
--- script/live_files/live_coding.rb
+++ script/live_files/live_coding.rb
@@ -1,8 +1,11 @@
 # An IRB shell for live coding. 
-# This flavor comes loaded with your sketch.
+# This flavor will either load up empty Ruby-Processing, 
+# or will start with your sketch.
 
 require 'irb'
-require ARGV[0]
+sketch = ARGV[0] || 'ruby-processing'
 ARGV[0] = nil
-APP = Processing::App.current
-IRB.start
+require sketch
+@app = Processing::App.current
+IRB.setup(__FILE__)
+IRB.start()
