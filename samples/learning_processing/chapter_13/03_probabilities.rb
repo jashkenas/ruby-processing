ef962a9e8c1997149426cf8b1f75b7b2dfc67854
Jeremy Ashkenas <jashkenas@gmail.com>
2008-12-11 23:37:14 -0500

    Adjusting some contributed sketches
    Former-commit-id: f6bf0c35f6e461a5311d1a12224822e9ad33985d
    Former-commit-id: 52720f51cd3dba1b5ff12699d1c667d79250ec84

--- samples/learning_processing/chapter_13/03_probabilities.rb
+++ samples/learning_processing/chapter_13/03_probabilities.rb
@@ -2,35 +2,29 @@ require 'ruby-processing'
 
 class ProbabilitiesSketch < Processing::App
   def setup
-    $app.background 255
-    $app.smooth
-    $app.noStroke
+    background 255
+    smooth
+    no_stroke
   end
 
   def draw
-
     # Probabilities for 3 different cases
-    #   These need to add up to 100%!
+    # These need to add up to 100%!
     red_prob = 0.60   # 60% chance of red color
-    green_prob = 0.10 # 10% chance of green color
-    blue_prob = 0.30  # 30% chance of blue color
+    green_prob = 0.70 # 10% chance of green color
+    blue_prob = 1.00  # 30% chance of blue color
 
     # Pick a random number between 0 and 1
     num = rand  
 
-    # If random number is less than .6
-    if num < red_prob  
-      $app.fill 255,53,2,150 
-      # If random number is between .6 and .7
-    elsif (num < green_prob + red_prob) 
-      $app.fill 156,255,28,150 
-      # All other cases (i.e. between .7 and 1.0)
-    else 
-      $app.fill 10,52,178,150 
+    case num
+      when (0.0..red_prob):         fill(255, 53,  2,   150)
+      when (red_prob..green_prob):  fill(156, 255, 28,  150)
+      when (green_prob..blue_prob): fill(10,  52,  178, 150)
     end
 
     # Draw ellipse
-    $app.ellipse random($app.width), random($app.height), 64, 64
+    ellipse rand(width), rand(height), 64, 64
   end
 
 end
