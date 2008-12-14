f512906ea137ca6cb49082c254d856a7df390066
Jeremy Ashkenas <jashkenas@gmail.com>
2008-12-14 17:37:14 -0500

    Merging in Martin's Learning Processing examples
    Former-commit-id: d7d09faa37e03acfd48c30b00be9847edc26fc74
    Former-commit-id: 84141b42c50f31f9c5d96c495f3e846beeb94645

--- samples/learning_processing/chapter_13/02_random_number_distribution.rb
+++ samples/learning_processing/chapter_13/02_random_number_distribution.rb
@@ -2,19 +2,23 @@ require 'ruby-processing'
 
 class RandomNumberDistributionSketch < Processing::App
   def setup
+    # An array to keep track of how often numbers are picked.
     @random_counts = Array.new(20, 0)
   end
 
   def draw
-    @random_counts[rand(@random_counts.size)] += 1
+    size = @random_counts.size
+    # Pick a random number and increase the count.
+    @random_counts[rand(size)] += 1
     background 255
     stroke 0
     fill 175
+    # Draw a rectangle to graph results
     @random_counts.each_with_index do |count, i|
-      rect i * width/@random_counts.size, 0, (width/@random_counts.size)-1, count  
+      rect i * width/size, 0, (width/size)-1, count  
     end
   end
 
 end
 
-RandomNumberDistributionSketch.new :title => "Random Number Distribution", :width => 400, :height => 400
+RandomNumberDistributionSketch.new :title => "Random Number Distribution", :width => 200, :height => 200
--- samples/learning_processing/chapter_13/07_wave.rb
+++ samples/learning_processing/chapter_13/07_wave.rb
@@ -10,23 +10,22 @@ class WaveSketch < Processing::App
   def draw
     background 255
     # Increment theta (try different values for " angular velocity " here)
-    @theta += 0.02;
-    noStroke
+    @theta += 0.02
+    no_stroke
     fill 0
     x = @theta
 
     # times is used to draw all the points along a sine wave (scaled to the pixel dimension of the window).
     20.times do |i|
       # Calculate y value based off of sine function
-      y = Math::sin(x) * height/2
+      y = sin(x) * height/2
       # Draw an ellipse
-      ellipse i * 20, y + height / 2, 32,32
+      ellipse i*10, y+height/2, 16, 16
       # Move along x-axis
       x += 0.2
     end
-
   end
 
 end
 
-WaveSketch.new :title => "Oscillation", :width => 400, :height => 400
+WaveSketch.new :title => "Oscillation", :width => 200, :height => 200
