471a35b105a02af37d514f4a2b51e4083a371b94
Jeremy Ashkenas <jashkenas@gmail.com>
2008-12-14 17:37:14 -0500

    Merging in Martin's Learning Processing examples
    Former-commit-id: 9c7a2e7d2b37cdb840325081a87507ac73595d8f
    Former-commit-id: b360b7f51a6910e7cd5330180cdcef5a494950a4

--- samples/learning_processing/chapter_13/06_oscillation.rb
+++ samples/learning_processing/chapter_13/06_oscillation.rb
@@ -3,7 +3,7 @@ require 'ruby-processing'
 class OscillationSketch < Processing::App
 
   def setup
-    @theta = 0
+    @theta = 0.0
     smooth
   end
 
@@ -12,7 +12,7 @@ class OscillationSketch < Processing::App
     # The output of the sin() function oscillates smoothly between 1 and 1. 
     # By adding 1 we get values between 0 and 2. 
     # By multiplying by 100, we get values between 0 and 200 which can be used as the ellipse's x location.
-    x = (Math::sin(@theta) + 1) * width/2
+    x = (sin(@theta) + 1) * width/2
 
     # with each cycle, increment theta
     @theta += 0.05
@@ -21,9 +21,9 @@ class OscillationSketch < Processing::App
 
     # draw the ellipse at the value produce by the sin
     line width/2, 0, x, height/2
-    ellipse x, height/2, 32, 32
+    ellipse x, height/2, 16, 16
   end
 
 end
 
-OscillationSketch.new :title => "Oscillation", :width => 400, :height => 400
+OscillationSketch.new :title => "Oscillation", :width => 200, :height => 200
