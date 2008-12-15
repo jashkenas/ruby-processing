c6db727cc3c7d5144a02da4b3a7b141bd2382250
Martin Stannard <mstannard@gmail.com>
2008-12-15 23:44:57 +1100

    Added the first 3 examples from chapter 14
    Former-commit-id: 8163adf8cb38b215e02ff43c3e0ef16e9c062b46
    Former-commit-id: a3f113b2c5832878e15c2eb82993ec7b3f69c38f

---
+++ samples/learning_processing/chapter_14/02_multiple_translations.rb
@@ -0,0 +1,38 @@
+require 'ruby-processing'
+
+class MultipleTranslationsSketch < Processing::App
+
+  def setup
+    smooth
+  end
+
+  def draw
+    background 255
+    stroke 0
+    fill 175
+
+    # Grab mouse coordinates,  constrained to window
+    mx = constrain mouseX, 0, width
+    my = constrain mouseY, 0, height
+
+    # Translate to the mouse location
+    translate mx, my
+    ellipse 0, 0, 8, 8
+
+    # Translate 100 pixels to the right
+    translate 100, 0
+    ellipse 0, 0, 8, 8
+
+    # Translate 100 pixels down
+    translate 0, 100
+    ellipse 0, 0, 8, 8
+
+    # Translate 100 pixels left
+    translate -100, 0
+    ellipse 0, 0, 8, 8
+
+  end
+
+end
+
+MultipleTranslationsSketch.new :title => "Multiple Translations",  :width => 400,  :height => 400
