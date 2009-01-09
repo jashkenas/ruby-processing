33d17327928045fb38ead3cfea2324a49ce010fc
Jeremy Ashkenas <jashkenas@gmail.com>
2009-01-09 00:10:08 -0500

    Optimization and Rubification of Chapter 15's Image Brightness sketch
    Former-commit-id: 5f346bc3eba491d859937f0796e0cb8e9057eb8a
    Former-commit-id: cb88f14be33dfa03ee58888183bfed392430e97c

--- samples/learning_processing/chapter_15/08_image_brightness.rb
+++ samples/learning_processing/chapter_15/08_image_brightness.rb
@@ -4,42 +4,21 @@ class ImageBrightnessSketch < Processing::App
 
   def setup
     @image = load_image 'sunflower.jpg'
+    # Let's grab the red, green and blue component of each pixel of the image to start with.
+    @image_pixels = @image.pixels.map {|p| [red(p), green(p), blue(p)]}
   end
 
   def draw
     # We calculate a multiplier ranging from 0.0 to 8.0 based on mouseX position. 
     # That multiplier changes the RGB value of each pixel.      
-    adjust_brightness = (mouseX.to_f / width) * 8.0 
-    # load the pixels array
-    load_pixels
-    # Two loops allow us to visit every column (x) and every row (y).
-    # Loop through every pixel column
-    width.times do |x|
-      # Loop through every pixel row
-      height.times do |y|
-        # Use the formula to find the 1D location
-        # The location in the pixel array is calculated via our formula: 1D pixel location = x + y * width
-        loc = x + y * width
-
-        # The functions red, green, and blue pull out the three color components from a pixel.
-        r = red(@image.pixels[loc]) 
-        g = green(@image.pixels[loc])
-        b = blue(@image.pixels[loc])
-
-        r *= adjust_brightness
-        g *= adjust_brightness
-        b *= adjust_brightness
-
-        # The RGB values are constrained between 0 and 255 before being set as a new color.      
-        r = constrain(r, 0, 255) 
-        g = constrain(g, 0, 255)
-        b = constrain(b, 0, 255)
-
-        # Make a new color and set pixel in the window
-        pixels[loc] = color(r, g, b)
-      end
+    adjustment = (mouse_x.to_f / width) * 8.0 
+    load_pixels # load the pixels array
+    
+    # And here's a modified Ruby way to do the Java equivalent
+    pixels.size.times do |i|
+      pixels[i] = color(*@image_pixels[i].map {|rgb| rgb * adjustment })
     end
-    # update the pixels on screen
+    
     update_pixels
   end
 
