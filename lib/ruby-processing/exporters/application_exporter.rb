70e3728f67352ab0a134681f02f0d3588ac47e19
Jeremy Ashkenas <jashkenas@gmail.com>
2009-01-15 21:27:17 -0500

    Refactor of both the Applet and Application exporters ... ahh, much better
    Former-commit-id: 9eaf6773f3d68c6f2bcc1bc53a622866cbf736e4
    Former-commit-id: 3a47aab7ae6ce91f735d5e652d5b18b9ca38f19c

--- lib/ruby-processing/exporters/application_exporter.rb
+++ lib/ruby-processing/exporters/application_exporter.rb
@@ -4,68 +4,84 @@ module Processing
   # Mac/Win/Nix Applications.
   class ApplicationExporter < BaseExporter
     
+    USAGE = <<-EOS
+        
+    The application exporter will generate a Mac application for you.
+    Usage: script/application <path_to_sketch>
+    Example: script/applet samples/jwishy.rb 
+      
+    EOS
+    
     def export!(sketch)
       # Check to make sure that the main file exists
       @main_file_path, @main_file, @main_folder = *get_main_file(sketch)
       usage( @main_file_path && File.exists?(@main_file_path) )
       
-      # Extract all the cool details.
-      @info = extract_information
-      hash_to_ivars @info
+      extract_information
       
-      # Make the appropriate directory
-      app_dir = "#{@title}.app"
-      remove_entry_secure app_dir if File.exists?(app_dir)
-      mkdir_p app_dir
+      compute_destination_name
       
-      # Copy over all the required files
-      prefix = "lib"
-      cp_r(Dir["#{RP5_ROOT}/lib/templates/application/{*,**}"], app_dir)
-      necessary_files = [@main_file_path]
-      necessary_files += Dir["#{RP5_ROOT}/lib/{*,**}"]
-      necessary_files += @real_requires
-      necessary_files << "#{@main_folder}/data" if File.exists?("#{@main_folder}/data")
-      necessary_files.uniq!
-      cp_r(necessary_files, File.join(app_dir, prefix))
-      cp_r(@libraries, File.join(app_dir, prefix, "library")) unless @libraries.empty?
+      wipe_and_recreate_destination
       
-      # Move the icon
-      potential_icon = Dir.glob(File.join(app_dir, prefix, "data/*.icns"))[0]
-      move(potential_icon, File.join(app_dir, "Contents/Resources/sketch.icns"), :force => true ) if potential_icon
+      copy_over_necessary_files
       
-      # Figure out the substitutions.
-      file_list = Dir.glob(app_dir + "{/**/*.{rb,jar},/data/*.*}").map {|f| f.sub(app_dir + "/", "")}
-      @class_path = file_list.map {|f| "$JAVAROOT/" + f.sub(prefix+"/", "") }.join(":")
-      @linux_class_path = file_list.map{|f| f.sub(prefix+"/", "")}.join(":")
-      @windows_class_path = file_list.map{|f| f.sub(prefix+"/", "").gsub('/', '\\')}.join(";")
+      calculate_substitutions
       
-      # Do it.
-      render_erb_in_path_with_binding(app_dir, binding, :delete => true)
-      rm Dir.glob(app_dir + "/**/*.{class,java}")
-      runnable = app_dir + "/" + File.basename(@main_file, ".rb")
-      move app_dir + "/run", runnable
-      move app_dir + "/run.bat", "#{runnable}.bat"
+      create_executables
+      
+      symlink_library_into_place
+    end
+    
+    def compute_destination_name
+      @dest = "#{@title}.app"
+    end
+    
+    def copy_over_necessary_files
+      @prefix = "lib"
+      cp_r(Dir["#{RP5_ROOT}/lib/templates/application/{*,**}"], @dest)
+      @necessary_files = [@main_file_path]
+      @necessary_files += Dir["#{RP5_ROOT}/lib/{*,**}"]
+      @necessary_files += @real_requires
+      @necessary_files << "#{@main_folder}/data" if File.exists?("#{@main_folder}/data")
+      @necessary_files.uniq!
+      cp_r(@necessary_files, File.join(@dest, @prefix))
+      cp_r(@libraries, File.join(@dest, @prefix, "library")) unless @libraries.empty?
+      # Then move the icon
+      potential_icon = Dir.glob(File.join(@dest, @prefix, "data/*.icns"))[0]
+      move(potential_icon, File.join(@dest, "Contents/Resources/sketch.icns"), :force => true ) if potential_icon
+    end
+    
+    def calculate_substitutions
+      file_list = Dir.glob(@dest + "{/**/*.{rb,jar},/data/*.*}").map {|f| f.sub(@dest + "/", "")}
+      @class_path = file_list.map {|f| "$JAVAROOT/" + f.sub(@prefix+"/", "") }.join(":")
+      @linux_class_path = file_list.map{|f| f.sub(@prefix+"/", "")}.join(":")
+      @windows_class_path = file_list.map{|f| f.sub(@prefix+"/", "").gsub('/', '\\')}.join(";")
+    end
+    
+    def create_executables
+      render_erb_in_path_with_binding(@dest, binding, :delete => true)
+      rm Dir.glob(@dest + "/**/*.java")
+      runnable = @dest + "/" + File.basename(@main_file, ".rb")
+      move @dest + "/run", runnable
+      move @dest + "/run.bat", "#{runnable}.bat"
       chmod 0755, runnable
       chmod 0755, "#{runnable}.bat"
-      cd app_dir + "/Contents/Resources"
+    end
+    
+    def symlink_library_into_place
+      cd @dest + "/Contents/Resources"
       # Poor ol' windows can't symlink.
       # TODO...
       win = RUBY_PLATFORM.match(/mswin/)
-      puts "\nSorry. Applications exported from Windows won't run on Macs...\n" if win 
+      puts "\n[warning] Applications exported from Windows won't run on Macs...\n" if win 
       ln_s('../../lib', 'Java') unless win
     end
     
     def usage(predicate)
-      unless predicate
-        puts <<-USAGE
-        
-      The application exporter will generate a Mac application for you.
-      Usage: script/application <path_to_sketch>
-      Example: script/applet samples/jwishy.rb 
-      
-      USAGE
-        exit
-      end
+      return if predicate
+      puts USAGE
+      exit
     end
+    
   end
 end
