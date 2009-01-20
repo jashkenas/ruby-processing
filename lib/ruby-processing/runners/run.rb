60cb8acaee6e460a392f83485ce527cd65d74d1f
Jeremy Ashkenas <jashkenas@gmail.com>
2009-01-20 00:04:50 -0500

    Fixing application exporting inconsistencies on windows and library loading in exported applications and adding a hidden java_args.txt option
    Former-commit-id: fd44a64f580df2d6307d7f55781ec6eb8dcfd7f5
    Former-commit-id: aff340fabfff072440d90483bb47957a1d8216a7

--- lib/ruby-processing/runners/run.rb
+++ lib/ruby-processing/runners/run.rb
@@ -1,5 +1,10 @@
 # Windows apps start life in the wrong folder.
-ARGV[1] = nil and Dir.chdir('lib') if ARGV[1] == '--windows-app'
+# TODO: Fix this crud as soon as possible.
+if ARGV[1] == '--windows-app'
+  ARGV[1] = nil
+  Dir.chdir('lib')
+  $__windows_app_mode__ = true
+end
 
 require "#{File.dirname(__FILE__)}/base.rb"
 require Processing::SKETCH_PATH
