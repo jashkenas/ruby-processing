6870bc01635e7328ccc1c5fc7d75552a6cd42ce6
Jeremy Ashkenas <jashkenas@gmail.com>
2009-03-10 21:29:30 -0400

    Need to define SKETCH_ROOT in the runners before loading ruby-processing ...
    Former-commit-id: c14e8dd606fbfb02788287c69f74c7863af12612
    Former-commit-id: 88780c9659ae43735ec2bf276e87df183bc51967

--- lib/ruby-processing/runners/base.rb
+++ lib/ruby-processing/runners/base.rb
@@ -1,8 +1,8 @@
 $LOAD_PATH << File.expand_path(File.dirname(__FILE__) + "/../../")
+SKETCH_ROOT = File.dirname(ARGV[0]) unless defined? SKETCH_ROOT
+
 require 'ruby-processing'
 require 'ruby-processing/app'
-
-SKETCH_ROOT = File.dirname(ARGV[0]) unless defined? SKETCH_ROOT
 
 module Processing
   SKETCH_PATH = ARGV[0]
