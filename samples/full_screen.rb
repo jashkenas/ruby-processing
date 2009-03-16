9bca7f0845378a20c5a8a685ff88086d8bf752c6
Jeremy Ashkenas <jashkenas@gmail.com>
2009-03-16 00:12:30 -0400

    moving some samples over to the new 'bare' style
    Former-commit-id: 17dfbeb895debb6932382302408a3f41c9d6cb9e
    Former-commit-id: cbd1402972f9b95ad1336fbc5a1b55d279dff5f5

--- samples/full_screen.rb
+++ samples/full_screen.rb
@@ -4,32 +4,27 @@
 # have the OpenGL library installed, you'll get *much* 
 # smoother and faster drawing.
 
-require 'ruby-processing'
+load_library :opengl
 
-class FullScreen < Processing::App
-  load_library :opengl
-  
-  def setup
-    library_loaded?(:opengl) ? render_mode(OPENGL) : render_mode(P3D)
-    no_stroke
-  end
-  
-  def draw
-    lights
-    background 0
-    fill 120, 160, 220
-    (width/100).times do |x|
-      (height/100).times do |y|
-        new_x, new_y = x * 100, y * 100
-        push_matrix
-        translate new_x + 50, new_y + 50
-        rotate_y(((mouse_x.to_f + new_x) / width) * Math::PI)
-        rotate_x(((mouse_y.to_f + new_y) / height) * Math::PI)
-        box 90
-        pop_matrix
-      end
+def setup
+  full_screen
+  library_loaded?(:opengl) ? render_mode(OPENGL) : render_mode(P3D)
+  no_stroke
+end
+
+def draw
+  lights
+  background 0
+  fill 120, 160, 220
+  (width/100).times do |x|
+    (height/100).times do |y|
+      new_x, new_y = x * 100, y * 100
+      push_matrix
+      translate new_x + 50, new_y + 50
+      rotate_y(((mouse_x.to_f + new_x) / width) * Math::PI)
+      rotate_x(((mouse_y.to_f + new_y) / height) * Math::PI)
+      box 90
+      pop_matrix
     end
   end
 end
-
-FullScreen.new(:full_screen => true, :title => "Full Screen", :width => 600, :height => 600)
