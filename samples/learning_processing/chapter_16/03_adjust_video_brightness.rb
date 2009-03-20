103002af9548e2a4596bdf4ed22554a000ae5d57
Juris Galang <jurisgalang@gmail.com>
2009-03-20 13:06:02 -0700

    Barren'd samples.
    Former-commit-id: 401cca198d073a50b6434459bcbcd709f13fb4d9
    Former-commit-id: 677ef471f360f1c45070b73289ec2345496ab8f5

--- samples/learning_processing/chapter_16/03_adjust_video_brightness.rb
+++ samples/learning_processing/chapter_16/03_adjust_video_brightness.rb
@@ -1,57 +1,52 @@
+#
 # Example 16-3: Adjust video brightness
-require 'ruby-processing'
+#
+load_library "video"
+import "processing.video" 
 
-class AdjustVideoBrightness < Processing::App
-  load_library "video"
-  import "processing.video" 
+def setup
+  size 320, 240
 
-  def setup
-    size 320, 240
-
-    # Initialize Capture object via Constructor
-    # video is 320 x 240, @15 fps
-    @video = Capture.new self, 320, 240, 15
-    background 0
-  end
-
-  def draw
-    # Check to see if a new frame is available
-    # If so, read it.
-    @video.read if @video.available?
-
-    loadPixels
-    @video.loadPixels
-
-    @video.width.times do |x|
-      @video.height.times do |y|
-        # Calculate the 1D location from a 2D grid
-        loc = x + y * @video.width
-
-        # Get the R,G,B values from image
-        r = red(@video.pixels[loc])
-        g = green(@video.pixels[loc])
-        b = blue(@video.pixels[loc])
-
-        # Calculate an amount to change brightness based on proximity to the mouse
-        maxdist          = 100
-        d                = dist(x, y, mouseX, mouseY)
-        adjustbrightness = (maxdist - d) / maxdist
-        r *= adjustbrightness
-        g *= adjustbrightness
-        b *= adjustbrightness
-
-        # Constrain RGB to make sure they are within 0-255 color range
-        r = constrain(r, 0, 255)
-        g = constrain(g, 0, 255)
-        b = constrain(b, 0, 255)
-
-        # Make a new color and set pixel in the window
-        pixels[loc] = color(r, g, b)
-      end
-    end
-    updatePixels
-  end
-
+  # Initialize Capture object via Constructor
+  # video is 320 x 240, @15 fps
+  @video = Capture.new self, 320, 240, 15
+  background 0
 end
 
-AdjustVideoBrightness.new :title => "03 Adjust Video Brightness"
+def draw
+  # Check to see if a new frame is available
+  # If so, read it.
+  @video.read if @video.available?
+
+  loadPixels
+  @video.loadPixels
+
+  @video.width.times do |x|
+    @video.height.times do |y|
+      # Calculate the 1D location from a 2D grid
+      loc = x + y * @video.width
+
+      # Get the R,G,B values from image
+      r = red(@video.pixels[loc])
+      g = green(@video.pixels[loc])
+      b = blue(@video.pixels[loc])
+
+      # Calculate an amount to change brightness based on proximity to the mouse
+      maxdist          = 100
+      d                = dist(x, y, mouseX, mouseY)
+      adjustbrightness = (maxdist - d) / maxdist
+      r *= adjustbrightness
+      g *= adjustbrightness
+      b *= adjustbrightness
+
+      # Constrain RGB to make sure they are within 0-255 color range
+      r = constrain(r, 0, 255)
+      g = constrain(g, 0, 255)
+      b = constrain(b, 0, 255)
+
+      # Make a new color and set pixel in the window
+      pixels[loc] = color(r, g, b)
+    end
+  end
+  updatePixels
+end
--- samples/learning_processing/chapter_18/01_user_input.rb
+++ samples/learning_processing/chapter_18/01_user_input.rb
@@ -1,39 +1,33 @@
+#
 # Example 18-1: User input
-require 'ruby-processing'
-
-class UserInput < Processing::App
-
-  def setup
-    size 300, 200
-    @typing = ""
-    @saved  = ""
-    textFont createFont "Arial", 16, true # set the font for text
-  end
-
-  def draw
-    background 255
-    indent = 25
-    fill 0 # Set the fill for text
-
-    # Display everything
-    text "Click in this applet and type. \nHit return to save what you typed. ", indent, 40
-    text @typing, indent, 90
-    text @saved, indent, 130  
-  end
-
-  def keyPressed
-    # If the return key is pressed, save the String and clear it
-    if key == "\n"
-      @saved  = @typing
-      @typing = "" # A String can be cleared by setting it equal to ""
-    else
-      # Otherwise, concatenate the String
-      # Each character typed by the user is added to the 
-      # end of the String variable.
-      @typing = @typing + key 
-    end
-  end
-
+#
+def setup
+  size 300, 200
+  @typing = ""
+  @saved  = ""
+  textFont createFont "Arial", 16, true # set the font for text
 end
 
-UserInput.new :title => "01 User Input"
+def draw
+  background 255
+  indent = 25
+  fill 0 # Set the fill for text
+
+  # Display everything
+  text "Click in this applet and type. \nHit return to save what you typed. ", indent, 40
+  text @typing, indent, 90
+  text @saved, indent, 130  
+end
+
+def keyPressed
+  # If the return key is pressed, save the String and clear it
+  if key == "\n"
+    @saved  = @typing
+    @typing = "" # A String can be cleared by setting it equal to ""
+  else
+    # Otherwise, concatenate the String
+    # Each character typed by the user is added to the 
+    # end of the String variable.
+    @typing = @typing + key 
+  end
+end
