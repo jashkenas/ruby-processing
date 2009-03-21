ec95dc31afd203fa0521c57530d87aeb6f9d820e
Jeremy Ashkenas <jashkenas@gmail.com>
2009-03-20 23:07:05 -0400

    merging in Juris' examples for chapters 16 and 17 of Learning Processing
    Former-commit-id: 8652c5b76681a9e62708ff12f115dc63f9135fb7
    Former-commit-id: 6f42ab9c1b5a8cec17bff5f83ff01496702c679f

--- samples/learning_processing/chapter_17/06_text_breaking_up.rb
+++ samples/learning_processing/chapter_17/06_text_breaking_up.rb
@@ -1,41 +1,34 @@
-# Example 17-6: Text breaking up 
 require 'ruby-processing'
 
 class TextBreakingUp < Processing::App
 
-
   def setup
     size 260, 200
+    fill 0
+    text_align LEFT
+    text_font create_font("Arial", 20, true)
     @message = "click mouse to shake it up"
-    textFont createFont("Arial", 20, true)
 
     # Create the array 
     @letters = []
 
     # Initialize Letters at the correct x location
     x = 16
-    @message.each_byte do |letter|
+    @message.each do |letter|
       # Letter objects are initialized with their location within 
       # the String as well as what character they should display.
-      @letters << Letter.new(x, 100, letter.chr)
-      x += textWidth(letter)
+      @letters << Letter.new(x, 100, letter)
+      x += text_width(letter)
     end
   end
 
   def draw
     background 255
     @letters.each do |letter|
-
-      # Display all letters
       letter.display
-
       # If the mouse is pressed the letters shake
       # If not, they return to their original location
-      if mouse_pressed?
-        letter.shake
-      else
-        letter.home
-      end
+      mouse_pressed? ? letter.shake : letter.home
     end
   end
 
@@ -43,40 +36,38 @@ end
 
 # A class to describe a single Letter
 class Letter
-  # The letter that this instance represent
+  # The letter that this instance represents
   attr_reader :letter
 
-  # The object knows its original " home " location
+  # The object knows its original "home" location...
   attr_reader :homex, :homey
 
   # As well as its current location
   attr_reader :x, :y
 
   def initialize(x, y, letter)
-    @homex  = @x = x
-    @homey  = @y = y
+    @home_x = @x = x
+    @home_y = @y = y
     @letter = letter
   end
 
   # Display the letter
   def display
-    $app.fill 0
-    $app.textAlign Processing::App::LEFT
     $app.text @letter, @x, @y
   end
 
   # Move the letter randomly
   def shake
-    @x += $app.random(-2, 2)
-    @y += $app.random(-2, 2)
+    @x += rand * 4 - 2
+    @y += rand * 4 - 2
   end
 
   # At any point, the current location can be set back to 
-  # the home location by calling the home() function.
+  # the home location by calling the home() method.
   def home
-    @x = @homex
-    @y = @homey
+    @x = @home_x
+    @y = @home_y
   end
 end
 
-TextBreakingUp.new :title => "06 Text Breaking Up"
+TextBreakingUp.new :title => "Text Breaking Up"
