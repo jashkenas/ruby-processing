11e92d607145a48019bb21a024724124d1bcf34b
Juris Galang <jurisgalang@gmail.com>
2009-03-23 17:56:08 -0700

    cosmetics.
    Former-commit-id: deb167475be33b2c4e401d26eed18f140f0e2f5d
    Former-commit-id: e365c8bcd3925daf7ba3df2999d1cfd1e568190a

--- samples/learning_processing/chapter_18/06_analyzing_king_lear.rb
+++ samples/learning_processing/chapter_18/06_analyzing_king_lear.rb
@@ -8,14 +8,14 @@ def setup
   # Where are we in the text (start later b/c Project Gutenberg has 
   # licensing info at beginning)
   @counter = 1509
-  
+
   # Load King Lear into an array of strings
   url     = "http://www.gutenberg.org/dirs/etext97/1ws3310.txt"
   rawtext = load_strings(url)
-  
+
   # Join the big array together as one long string
   everything = rawtext.join
-  
+
   # All the lines in King Lear are first joined as one big String and 
   # then split up into an array of individual words. 
   # Note the use of splitTokens() since we are using spaces and 
@@ -28,16 +28,16 @@ end
 
 def draw
   background 255
-  
+
   # Pick one word from King Lear
   theword = @kinglear[@counter]
-  
+
   # Count how many times that word appears in King Lear
   total = 0
   @kinglear.length.times do |i|
-    total +=  1 if theword == @kinglear[i]
+    total += 1 if theword == @kinglear[i]
   end
-  
+
   # Display the text and total times the word appears
   fill 0
   text theword, 10, 90
@@ -45,7 +45,7 @@ def draw
   stroke 0
   fill 175
   rect 10, 50, total / 4, 20
-  
+
   # Move onto the next word
   @counter = (@counter + 1) % @kinglear.length
 end
