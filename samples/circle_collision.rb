d25b747a3875d54446effbe276c95395ce9287bb
Jeremy Ashkenas <jashkenas@gmail.com>
2009-04-01 22:29:46 -0400

    changed the circle collision example to use inner classes
    Former-commit-id: f33ef074535d6136528550e1d66b4fac913d6c4a
    Former-commit-id: 51615b2a8ac14e7bb6cfac6babea793795e69e16

--- samples/circle_collision.rb
+++ samples/circle_collision.rb
@@ -1,52 +1,45 @@
 # Based on http://processing.org/learning/topics/circlecollision.html
 # by Joe Holt
 
-require 'ruby-processing'
-
-class Vect2D
-  attr_accessor :vx, :vy
-  def initialize(vx = 0.0, vy = 0.0)
-    @vx = vx
-    @vy = vy
-  end
-end
-
-
-class Ball
-  attr_accessor :x, :y, :r, :m, :vec
-  def initialize(r = 0.0, vec = nil, x = 0.0, y = 0.0)
-    @x = x
-    @y = y
-    @r = r
-    @m = r*0.1
-    @vec = vec
-  end
-
-  def draw
-    r = @r * 2
-    P.ellipse @x, @y, r, r
-    @px = @x
-    @py = @y
-  end
-
-  def erase
-    r = @r * 2
-    P.rect @px, @py, r, r
-  end
-end
-
-
 class Sketch < Processing::App
+  
+  # This inner class demonstrates the use of Ruby-Processing's emulation of
+  # Java inner classes. The Balls are able to call Processing::App methods.
+  class Ball
+    attr_accessor :x, :y, :r, :m, :vec
+    def initialize(r = 0.0, vec = nil, x = 0.0, y = 0.0)
+      @x, @y, @r = x, y, r
+      @m = r * 0.1
+      @vec = vec
+    end
+    
+    def move
+      @x += @vec.x
+      @y += @vec.y
+    end
+  
+    def draw
+      r = @r * 2
+      ellipse @x, @y, r, r
+      @px, @py = @x, @y
+    end
+  
+    def erase
+      r = @r * 2
+      rect @px, @py, r, r
+    end
+  end
+  
 
   def setup
     smooth
-    noStroke
-    frameRate 30
-    rectMode RADIUS
+    no_stroke
+    frame_rate 30
+    rect_mode RADIUS
 
     @balls = []
-    5.times { @balls << Ball.new(10, Vect2D.new(2.15, -1.35), *emptySpace(15)) }
-    2.times { @balls << Ball.new(40, Vect2D.new(-1.65, 0.42), *emptySpace(45)) }
+    5.times { @balls << Ball.new(10, PVector.new(2.15, -1.35), *empty_space(15)) }
+    2.times { @balls << Ball.new(40, PVector.new(-1.65, 0.42), *empty_space(45)) }
 
     @frame_time = nil
     @frame_count = 0
@@ -55,9 +48,7 @@ class Sketch < Processing::App
 
   def draw
     t = Time.now
-    if @frame_time
-        fps = 1.0 / (t - @frame_time)
-    end
+    fps = 1.0 / (t - @frame_time) if @frame_time
     @frame_time = t
     @frame_count += 1
 
@@ -72,19 +63,17 @@ class Sketch < Processing::App
     # move the balls 
     fill 240
     @balls.each do |ball|
-      ball.x += ball.vec.vx
-      ball.y += ball.vec.vy
+      ball.move
       ball.draw
-      checkBoundaryCollision ball
+      check_boundary_collision ball
     end
-    checkObjectCollisions
-    1 * 20
+    check_object_collisions
   end
 
 
-  def emptySpace(r)
+  def empty_space(r)
     x = y = nil
-    while !x || !emptySpace?(x, y, r) do
+    while !x || !empty_space?(x, y, r) do
       x = rand(width)
       y = rand(height)
     end
@@ -92,7 +81,7 @@ class Sketch < Processing::App
   end
 
 
-  def emptySpace?(x, y, r)
+  def empty_space?(x, y, r)
     @balls.each do |ball|
       vx = x - ball.x
       vy = y - ball.y
@@ -103,7 +92,7 @@ class Sketch < Processing::App
   end
 
 
-  def checkObjectCollisions
+  def check_object_collisions
 
     (0...(@balls.length)).each do |ia|
       ((ia+1)...(@balls.length)).each do |ib|
@@ -112,15 +101,15 @@ class Sketch < Processing::App
         bb = @balls[ib]
   
         # get distances between the balls components
-        bVect = Vect2D.new
-        bVect.vx = bb.x - ba.x
-        bVect.vy = bb.y - ba.y
+        bVect = PVector.new
+        bVect.x = bb.x - ba.x
+        bVect.y = bb.y - ba.y
 
         # calculate magnitude of the vector separating the balls
-        bVectMag = sqrt(bVect.vx * bVect.vx + bVect.vy * bVect.vy)
+        bVectMag = sqrt(bVect.x * bVect.x + bVect.y * bVect.y)
         next if bVectMag >= ba.r + bb.r
         # get angle of bVect
-        theta  = atan2(bVect.vy, bVect.vx)
+        theta  = atan2(bVect.y, bVect.x)
         # precalculate trig values
         sine = sin(theta)
         cosine = cos(theta)
@@ -134,32 +123,32 @@ class Sketch < Processing::App
         # bTemp[0].x and bTemp[0].y will initialize
         # automatically to 0.0, which is what you want
         # since bb will rotate around ba
-        bTemp[1].x  = cosine * bVect.vx + sine * bVect.vy
-        bTemp[1].y  = cosine * bVect.vy - sine * bVect.vx
+        bTemp[1].x  = cosine * bVect.x + sine * bVect.y
+        bTemp[1].y  = cosine * bVect.y - sine * bVect.x
 
         # rotate Temporary velocities
-        vTemp = [Vect2D.new, Vect2D.new]
-        vTemp[0].vx  = cosine * ba.vec.vx + sine * ba.vec.vy
-        vTemp[0].vy  = cosine * ba.vec.vy - sine * ba.vec.vx
-        vTemp[1].vx  = cosine * bb.vec.vx + sine * bb.vec.vy
-        vTemp[1].vy  = cosine * bb.vec.vy - sine * bb.vec.vx
+        vTemp = [PVector.new, PVector.new]
+        vTemp[0].x  = cosine * ba.vec.x + sine * ba.vec.y
+        vTemp[0].y  = cosine * ba.vec.y - sine * ba.vec.x
+        vTemp[1].x  = cosine * bb.vec.x + sine * bb.vec.y
+        vTemp[1].y  = cosine * bb.vec.y - sine * bb.vec.x
 
         # Now that velocities are rotated, you can use 1D
         # conservation of momentum equations to calculate 
         # the final velocity along the x-axis.
-        vFinal = [Vect2D.new, Vect2D.new]
+        vFinal = [PVector.new, PVector.new]
         # final rotated velocity for ba
-        vFinal[0].vx = ((ba.m - bb.m) * vTemp[0].vx + 2 * bb.m * 
-          vTemp[1].vx) / (ba.m + bb.m)
-        vFinal[0].vy = vTemp[0].vy
+        vFinal[0].x = ((ba.m - bb.m) * vTemp[0].x + 2 * bb.m * 
+          vTemp[1].x) / (ba.m + bb.m)
+        vFinal[0].y = vTemp[0].y
         # final rotated velocity for ba
-        vFinal[1].vx = ((bb.m - ba.m) * vTemp[1].vx + 2 * ba.m * 
-          vTemp[0].vx) / (ba.m + bb.m)
-        vFinal[1].vy = vTemp[1].vy
+        vFinal[1].x = ((bb.m - ba.m) * vTemp[1].x + 2 * ba.m * 
+          vTemp[0].x) / (ba.m + bb.m)
+        vFinal[1].y = vTemp[1].y
 
         # hack to avoid clumping
-        bTemp[0].x += vFinal[0].vx
-        bTemp[1].x += vFinal[1].vx
+        bTemp[0].x += vFinal[0].x
+        bTemp[1].x += vFinal[1].x
 
         # Rotate ball positions and velocities back
         # Reverse signs in trig expressions to rotate 
@@ -178,36 +167,34 @@ class Sketch < Processing::App
         ba.y = ba.y + bFinal[0].y
 
         # update velocities
-        ba.vec.vx = cosine * vFinal[0].vx - sine * vFinal[0].vy
-        ba.vec.vy = cosine * vFinal[0].vy + sine * vFinal[0].vx
-        bb.vec.vx = cosine * vFinal[1].vx - sine * vFinal[1].vy
-        bb.vec.vy = cosine * vFinal[1].vy + sine * vFinal[1].vx
+        ba.vec.x = cosine * vFinal[0].x - sine * vFinal[0].y
+        ba.vec.y = cosine * vFinal[0].y + sine * vFinal[0].x
+        bb.vec.x = cosine * vFinal[1].x - sine * vFinal[1].y
+        bb.vec.y = cosine * vFinal[1].y + sine * vFinal[1].x
       end
     end
 
   end
 
 
-  def checkBoundaryCollision(ball)
-
+  def check_boundary_collision(ball)
     if ball.x > width-ball.r
       ball.x = width-ball.r
-      ball.vec.vx *= -1
+      ball.vec.x *= -1
     elsif ball.x < ball.r
       ball.x = ball.r
-      ball.vec.vx *= -1
+      ball.vec.x *= -1
     end
     if ball.y > height-ball.r
       ball.y = height-ball.r
-      ball.vec.vy *= -1
+      ball.vec.y *= -1
     elsif ball.y < ball.r
       ball.y = ball.r
-      ball.vec.vy *= -1
+      ball.vec.y *= -1
     end
-
   end
 
 end
 
 
-P = Sketch.new(:width => 400, :height => 400, :title => "CircleCollision2")
+Sketch.new(:width => 400, :height => 400, :title => "CircleCollision2")
