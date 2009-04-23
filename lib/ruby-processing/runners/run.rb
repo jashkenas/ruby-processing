89eb7d167d5a857dc7915ae3726262dc500cad0a
Jeremy Ashkenas <jashkenas@gmail.com>
2009-04-22 21:06:24 -0400

    fixing up applet and app exporting for windows ... they were lost in the wilderness for a bit
    Former-commit-id: 2cce9b6f360857dd8cf44e110cf05a1870872ca9
    Former-commit-id: ca9a1ca31461cc15e84d8b80f81e6f38fde38abe

--- lib/ruby-processing/runners/run.rb
+++ lib/ruby-processing/runners/run.rb
@@ -1,18 +1,7 @@
-# Windows apps start life in the wrong folder.
-# TODO: Fix this crud as soon as possible.
-if ARGV[1] == '--windows-app'
-  ARGV[1] = nil
-  Dir.chdir('lib')
-  $__windows_app_mode__ = true
-end
-
-# TODO: this is also crud. Windows applets are having serious
+# TODO: this is crud. Windows applets are having serious
 # trouble with absolute paths.
 
-if defined?(JRUBY_APPLET)
-  require 'ruby-processing/runners/base.rb'
-else
-  require "#{File.dirname(__FILE__)}/base.rb"
-end
+root = defined?(JRUBY_APPLET) ? 'ruby-processing/runners' : File.dirname(__FILE__)
+require "#{root}/base.rb"
 
 Processing.load_and_run_sketch
