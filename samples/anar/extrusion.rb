34abfec29612861b78ff370965a28249356c362d
Jeremy Ashkenas <jashkenas@gmail.com>
2009-12-22 08:03:40 -0500

    merging in monkstone's anar library examples, edits for extrusion.rb
    Former-commit-id: c1bb9daed4328468648f760d898406784788b7ac
    Former-commit-id: d58133f71143b78fdf51e183e7eaef71eccf41fd

---
+++ samples/anar/extrusion.rb
@@ -0,0 +1,49 @@
+# extrude.rb is a processing sketch that shows
+# how you can use the anar library in Ruby-Processing.
+
+load_libraries 'anar', 'opengl'
+import "anar"
+import "processing.opengl"
+full_screen
+
+def setup
+  configure_opengl
+  configure_anar
+  @points = [
+    [100,  110,  20],
+    [110,  100,  40],
+    [110,  90,   60],
+    [90,   90,   90]
+  ]
+  create_extrusion
+end
+
+def configure_opengl
+  render_mode OPENGL
+  hint ENABLE_OPENGL_4X_SMOOTH     # optional
+  hint DISABLE_OPENGL_ERROR_REPORT # optional
+end
+
+def configure_anar
+  Anar.init self
+  Anar.draw_axis true
+end
+
+def create_extrusion
+  pts = Pts.new
+  @points.each {|xyz| pts.add(Anar.Pt(*xyz)) } # make a line out of points
+  face = Star.new(50,100,5)                    # make a face
+  @extrusion = Extrude.new(face, pts)          # extrude the face along the line
+  Anar.cam_target(@extrusion)
+  Anar.sliders(@extrusion)
+end
+
+def draw
+  background 155
+  @extrusion.draw
+end
+
+# show or hide the sliders
+def key_pressed
+  Anar.sliders_toggle
+end
