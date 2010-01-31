d0f29fc19d27a079ff89e93cd780dca42e2d7bd0
Jeremy Ashkenas <jashkenas@gmail.com>
2010-01-31 14:25:57 -0500

    rearranging hilbert into a library, and merging martin's samples
    Former-commit-id: 5e4507afb0582a196d90a1dea464db61294de8d0
    Former-commit-id: 4d5beb46c70dd36606feb28954a9520f6d911f77

--- samples/peasy_cam/hello_peasy.rb
+++ samples/peasy_cam/hello_peasy.rb
@@ -1,29 +1,29 @@
-load_libraries 'PeasyCam'
-import 'peasy' 
+load_library 'PeasyCam'
+import 'peasy'
 
 attr_reader :cam
 
-def setup() 
-  size(200, 200, P3D)
-  configure_camera()
+def setup
+  size 200, 200, P3D
+  configure_camera
 end
 
-def configure_camera()
+def configure_camera
   @cam = PeasyCam.new(self, 100)
-  cam.set_minimum_distance(50)
-  cam.set_maximum_distance(500)
+  cam.set_minimum_distance 50
+  cam.set_maximum_distance 500
 end
 
-def draw() 
-  rotate_x(-0.5)
-  rotate_y(-0.5)
-  background(0)
-  fill(255, 0, 0)
-  box(30)
-  push_matrix()
-  translate(0, 0, 20)
-  fill(0, 0, 255)
-  box(5)
-  pop_matrix()
+def draw
+  rotate_x -0.5
+  rotate_y -0.5
+  background 0
+  fill 255, 0, 0
+  box 30
+  push_matrix
+  translate 0, 0, 20
+  fill 0, 0, 255
+  box 5
+  pop_matrix
 end
 
--- samples/peasy_cam/library/hilbert/hilbert.rb
+++ samples/peasy_cam/library/hilbert/hilbert.rb
@@ -1,13 +1,49 @@
 ############################
-# hilbert.rb
+# Non-stochastic grammar
+# with unique premise/rules
+############################
+class Grammar
+  attr_accessor :axiom, :rules
+
+  def initialize axiom
+    @axiom = axiom
+    @rules = Hash.new
+  end
+
+  def add_rule premise, rule
+    rules.store(premise, rule)
+  end
+
+  ##########################################
+  # replace each pre char with a unique rule
+  ##########################################
+  def new_production production
+    production.gsub!(/./) { |c| (r = @rules[c]) ? r : c }
+  end
+
+  ##########################################
+  # control the number of iterations
+  # default 0, returns the axiom
+  ##########################################
+  def generate repeat = 0
+    prod = axiom
+    repeat.times do
+      prod = new_production prod
+    end
+    return prod
+  end
+end
+
+############################
+# Hilbert Curves
 ###########################
 class Hilbert
   include Processing::Proxy
 
-  attr_reader :grammar, :axiom, :production, :premis, :rule, 
+  attr_reader :grammar, :axiom, :production, :premis, :rule,
   :theta, :scale_factor, :distance, :phi, :len
-  
-  def initialize(len) 
+
+  def initialize(len)
     @axiom = "X"
     @grammar = Grammar.new(axiom)
     @production = axiom
@@ -22,14 +58,14 @@ class Hilbert
   end
 
   def render()
-    translate(-len/42, len/42, -len/42)  # use the "answer?" to center the Hilbert 
-    fill(0, 75, 152) 
-    light_specular(204, 204, 204) 
-    specular(255, 255, 255) 
-    shininess(1.0) 
-    production.scan(/./) do |ch| 
+    translate(-len/42, len/42, -len/42)  # use the "answer?" to center the Hilbert
+    fill(0, 75, 152)
+    light_specular(204, 204, 204)
+    specular(255, 255, 255)
+    shininess(1.0)
+    production.scan(/./) do |ch|
       case(ch)
-      when "F"     
+      when "F"
         translate(0, distance/-2, 0)
         box(distance/9 , distance, distance/9)
         translate(0, distance/-2, 0)
@@ -55,11 +91,9 @@ class Hilbert
   # create grammar from axiom and
   # rules (adjust scale)
   ##############################
-  
+
   def create_grammar(gen)
     @distance *= 0.5**gen
     @production = @grammar.generate gen
   end
 end
-
-
