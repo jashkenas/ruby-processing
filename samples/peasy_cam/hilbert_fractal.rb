0d5ffa8e55c75c186b569ba3c98623f8a49613e3
Jeremy Ashkenas <jashkenas@gmail.com>
2010-01-31 14:25:57 -0500

    rearranging hilbert into a library, and merging martin's samples
    Former-commit-id: 2dc4a7912346d93b1d161686d90e009a359a5a5b
    Former-commit-id: 94b6684a04ee0159c5dbc402b2134ce79bdd1d2b

--- samples/peasy_cam/hilbert_fractal.rb
+++ samples/peasy_cam/hilbert_fractal.rb
@@ -4,38 +4,37 @@
 # Best if you've got opengl
 ########################################################
 
-require 'hilbert'
-
 class Hilbert_Test < Processing::App
   full_screen # NB: All distances are relative to screen height
-  load_libraries 'grammar', 'PeasyCam', 'opengl'
-  import 'peasy' 
-  import "processing.opengl" if library_loaded? "opengl"  
+  load_libraries 'hilbert', 'PeasyCam', 'opengl'
+  import 'peasy'
+  import "processing.opengl" if library_loaded? "opengl"
   attr_reader :hilbert, :cam
-  def setup() 
+
+  def setup
     library_loaded?(:opengl) ? configure_opengl : render_mode(P3D)
     configure_peasycam
     @hilbert = Hilbert.new(height)
-    hilbert.create_grammar(3)
-    no_stroke()
+    hilbert.create_grammar 3
+    no_stroke
   end
-  
+
   def configure_peasycam
-    cam = PeasyCam.new(self, height/10)  
-    cam.set_minimum_distance(height/10)
-    cam.set_maximum_distance(height/2)
+    cam = PeasyCam.new self, height / 6.5
+    cam.set_minimum_distance height / 10
+    cam.set_maximum_distance height
   end
-  
+
   def configure_opengl
     render_mode OPENGL
     hint ENABLE_OPENGL_4X_SMOOTH     # optional
     hint DISABLE_OPENGL_ERROR_REPORT # optional
   end
 
-  def draw() 
-    background(0)
-    lights()
-    hilbert.render()
+  def draw
+    background 0
+    lights
+    hilbert.render
   end
 end
 
