dfd7835d818d79761472aea0a15c9e1ce87544c3
Guillaume Pierronnet <guillaume.pierronnet@gmail.com>
2011-09-20 17:40:31 +0200

    load libraries from sketchbook librairies folder (like Processing)
    Former-commit-id: 025343cddabb5d21b1b96f1fe944aa2afb34c069
    Former-commit-id: 78e866d2cdede4b0032952f5bba9b2dae8cc3d01

--- lib/ruby-processing/app.rb
+++ lib/ruby-processing/app.rb
@@ -130,28 +130,79 @@ module Processing
     # P.S. -- Loading libraries which include native code needs to
     # hack the Java ClassLoader, so that you don't have to
     # futz with your PATH. But it's probably bad juju.
-    def self.load_java_library(dir)
-      dir = dir.to_sym
-      return true if @@loaded_libraries[dir]
-      return @@loaded_libraries[dir] = !!(JRUBY_APPLET.get_parameter("archive").match(%r(#{dir}))) if Processing.online?
-      local_path = "#{SKETCH_ROOT}/library/#{dir}"
-      gem_path = "#{RP5_ROOT}/library/#{dir}"
+    def self.load_java_library(library_name)
+      library_name = library_name.to_sym
+      return true if @@loaded_libraries[library_name]
+      return @@loaded_libraries[library_name] = !!(JRUBY_APPLET.get_parameter("archive").match(%r(#{library_name}))) if Processing.online?
+      local_path = "#{SKETCH_ROOT}/library/#{library_name}"
+      gem_path = "#{RP5_ROOT}/library/#{library_name}"
       path = File.exists?(local_path) ? local_path : gem_path
       jars = Dir["#{path}/**/*.jar"]
+      sketchbook_libraries_path = sketchbook_path + "/libraries"
+      if File.exists?(sketchbook_libraries_path)
+        jars.concat(Dir["#{sketchbook_libraries_path}/#{library_name}/library/*.jar"])
+      end
       return false if jars.empty?
       jars.each {|jar| require jar }
-      # Here goes...
-      library_path = java.lang.System.getProperty("java.library.path")
-      new_library_path = [path, "#{path}/library", library_path].join(java.io.File.pathSeparator)
+
+      library_paths = [path, "#{path}/library"]
+      library_paths.concat(platform_specific_library_paths.collect { |d| "#{path}/library/#{d}" } )
+      library_paths.concat(platform_specific_library_paths.collect { |d| "#{sketchbook_libraries_path}/#{library_name}/library/#{d}" } )
+      #p library_paths
+      library_paths = library_paths.select do |path|
+        test(?d, path)
+      end
+
+      #p library_paths
+      library_paths << java.lang.System.getProperty("java.library.path")
+      new_library_path = library_paths.join(java.io.File.pathSeparator)
+
       java.lang.System.setProperty("java.library.path", new_library_path)
+
       field = java.lang.Class.for_name("java.lang.ClassLoader").get_declared_field("sys_paths")
       if field
         field.accessible = true
         field.set(java.lang.Class.for_name("java.lang.System").get_class_loader, nil)
       end
-      return @@loaded_libraries[dir] = true
+      return @@loaded_libraries[library_name] = true
     end
 
+    def self.sketchbook_path
+      preferences_paths = []
+      sketchbook_paths = []
+      ["Application Data/Processing", "AppData/Roaming/Processing", 
+       "Library/Processing", "Documents/Processing", 
+       ".processing", "sketchbook"].each do |prefix|
+        path = "#{ENV["HOME"]}/#{prefix}"
+        pref_path = path+"/preferences.txt"
+        if test(?f, pref_path)
+          preferences_paths << pref_path
+        end
+        if test(?d, path)
+          sketchbook_paths << path
+        end
+      end
+      if !preferences_paths.empty?
+        matched_lines = File.readlines(preferences_paths.first).grep(/^sketchbook\.path=(.+)/) { $1 }
+        return matched_lines.first
+      else
+        sketchbook_paths.first
+      end
+    end
+
+    def self.platform_specific_library_paths
+      bits = "32"
+      if java.lang.System.getProperty("sun.arch.data.model") == "64" || 
+         java.lang.System.getProperty("java.vm.name").index("64")
+        bits = "64"
+      end
+
+      match_string, platform = {"Mac" => "macosx", "Linux" => "linux", "Windows" => "windows" }.detect do |string, platform_|
+        java.lang.System.getProperty("os.name").index(string)
+      end
+      platform ||= "other"
+      [ platform, platform+bits ]
+    end
 
     def self.has_slider(*args) #:nodoc:
       raise "has_slider has been replaced with a nicer control_panel library. Check it out."
