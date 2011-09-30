fc9ef05acd91a985837f68cb8ce213e8f8683ecb
Guillaume Pierronnet <guillaume.pierronnet@gmail.com>
2011-09-30 13:10:30 +0200

    test suite now uses Thread queue
    Former-commit-id: 0e73ed5393022568e8ec8c938ce6acb06ea90c2f
    Former-commit-id: d14a3387803fa286dbb25cf0510ed42960babc34

--- test/rp5_test.rb
+++ test/rp5_test.rb
@@ -2,14 +2,15 @@ require "test/unit"
 require "fileutils"
 require "tempfile"
 require "timeout"
+require "thread"
 
 Dir.chdir(File.dirname(__FILE__))
 
 class Rp5Test < Test::Unit::TestCase
   OUTPUT_FILE = File.join(Dir.pwd, "output.txt")
-  
+
   def test_normal
-    output = write_and_run_sketch <<EOF
+    queue = write_and_run_sketch <<EOF
 def setup
   size(300, 300)
   frame_rate(10)
@@ -22,11 +23,13 @@ def draw
   end
 end
 EOF
-    assert_equal "ok\n"*10, output
+    10.times do
+      assert_equal "ok", queue.pop
+    end
   end
 
   def test_opengl
-    output = write_and_run_sketch <<EOF
+    queue = write_and_run_sketch <<EOF
 load_library "opengl"
 
 def setup
@@ -38,11 +41,11 @@ def draw
   exit
 end
 EOF
-    assert_equal "ok\n", output
+    assert_equal "ok", queue.pop
   end
 
   def test_setup_exception
-    output = write_and_run_sketch <<EOF
+    queue = write_and_run_sketch <<EOF
 def setup
   size(300, 300)
   unknown_method()
@@ -51,11 +54,11 @@ end
 def draw
 end
 EOF
-    assert output.index("undefined method `unknown_method'")
+    assert queue.pop.index("undefined method `unknown_method'")
   end
 
   def test_draw_exception
-    output = write_and_run_sketch <<EOF
+    queue = write_and_run_sketch <<EOF
 def setup
   size(300, 300)
 end
@@ -64,11 +67,11 @@ def draw
   unknown_method()
 end
 EOF
-    assert output.index("undefined method `unknown_method'")
+    assert queue.pop.index("undefined method `unknown_method'")
   end
   
   def test_inner_classes_proxy
-    output = write_and_run_sketch <<EOF
+    queue = write_and_run_sketch <<EOF
 def setup
   size(300, 300)
   begin
@@ -85,7 +88,7 @@ class MyInnerClass
   end
 end
 EOF
-    assert_equal "300\n", output
+    assert_equal "300", queue.pop
   end
 
   def test_rp5_watch
@@ -122,20 +125,27 @@ EOF
   end
 
   def write_and_run_sketch(sketch_content)
-    Tempfile.open("rp5_tester") do |tf|
-      tf.write(sketch_content)
-      tf.close
-      #FileUtils.cp(tf.path, "/tmp/sketch.rb", :verbose => true)
-      output = nil
-      begin
-        Timeout.timeout(15) do 
-          output = `../bin/rp5 run #{tf.path} 2>&1`
+    queue = Queue.new
+    Thread.new do
+      Tempfile.open("rp5_tester") do |tf|
+        tf.write(sketch_content)
+        tf.close
+        #FileUtils.cp(tf.path, "/tmp/sketch.rb", :verbose => true)
+        output = nil
+        begin
+          Timeout.timeout(15) do 
+            open("|../bin/rp5 run #{tf.path}", "r") do |io|
+              while l = io.gets
+                queue.push(l.chop)
+              end
+            end
+          end
+          assert $?.exited?
+        rescue Timeout::Error
+          assert false, "rp5 timed out"
         end
-        assert $?.exited?
-      rescue Timeout::Error
-        assert false, "rp5 timed out"
       end
-      return output
     end
+    return queue
   end
 end
