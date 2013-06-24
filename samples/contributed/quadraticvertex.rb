a81e3368e45ce43907b28e00af0f63a32ca35077
monkstone <martin_p@lineon:e.net>
2013-06-24 19:51:14 +0100

    dry quadraticvertex.rb
    Former-commit-id: 8e64e6d8e6f0a1f2ff7493888333ff2364edc790
    Former-commit-id: 1a245b478e416216ed9f1f67d553a2466fa18ee8

--- samples/contributed/quadraticvertex.rb
+++ samples/contributed/quadraticvertex.rb
@@ -2,39 +2,38 @@
 # Part of a series of articles on Processing 2.
 # Blog post here:
 # http://vormplus.be/blog/article/drawing-shapes-with-quadratic-vertices
-# translated for ruby-processing by Martin Prout (needs some drying)
+# translated for ruby-processing by Martin Prout 
 # Note how to specify fill and background using hexadecimal string for color, 
 # this is different from vanilla processing
 #
 
-attr_reader :debug, :save_one 
+attr_reader :debug, :save_one, :step_angle, :cr, :data
 
 def setup
   size 450, 320 
   @debug = false
   @save_one = false
+  @step_angle = TWO_PI / 6
+  @data = Array.new(6){ |i| i + 1 }
+  @data.unshift(0)
   smooth 8
 end
 
 def draw
   background color('#BDF018')
-  translate width/2, height/2 
-  step_angle = TWO_PI / 6
+  translate width / 2, height / 2 
+  
   fill color('#ffffff')
   no_stroke
-  cr = map(mouse_x, 0, width, 20, 200)
+  @cr = map(mouse_x, 0, width, 20, 200)
   begin_shape
-  7.times do |i| 
-    x = cos(step_angle * i) * 100
-    y = sin(step_angle * i) * 100
-    cx = cos(step_angle * i - (step_angle / 2 )) * cr
-    cy = sin(step_angle * i - (step_angle / 2 )) * cr    
+  data.each { |i|  
     if (i == 0) 
-      vertex x, y 
+      vertex cos_x(i), sin_y(i) 
     else 
-      quadratic_vertex cx, cy, x, y 
+      quadratic_vertex cos_cx(i), sin_cy(i), cos_x(i), sin_y(i)
     end
-  end
+  }
   end_shape(CLOSE)
   
   if (debug)    
@@ -44,37 +43,45 @@ def draw
     stroke(0)
     
     begin_shape
-    7.times do |i|      
-      x = cos(step_angle * i) * 100
-      y = sin(step_angle * i) * 100      
-      cx = cos(step_angle * i - (step_angle / 2)) * cr
-      cy = sin(step_angle * i - (step_angle / 2)) * cr      
+    data.each { |i|    
       if ( i > 0 ) 
-        vertex cx, cy 
+        vertex cos_cx(i), sin_cy(i) 
       end
-      vertex x, y 
-    end
+      vertex cos_x(i), sin_y(i) 
+    }
     end_shape CLOSE
     
     # draw points
     stroke_weight 8
     
-    7.times do |i|       
-      x = cos(step_angle * i ) * 100
-      y = sin(step_angle * i ) * 100      
-      cx = cos(step_angle * i - (step_angle / 2 )) * cr
-      cy = sin(step_angle * i - (step_angle / 2 )) * cr      
+    data.each { |i|  
       stroke 0 
-      point x, y       
+      point cos_x(i), sin_y(i)      
       stroke 255, 0, 0 
-      point cx, cy 
-    end
+      point cos_cx(i), sin_cy(i) 
+    }
   end
   
   if (save_one) 
     save_frame("images/quadraticvertex-#####.png")
     @save_one = false
   end
+end
+
+def cos_x(n)
+  cos(step_angle * n) * 100
+end
+
+def sin_y(n)
+  sin(step_angle * n) * 100
+end
+
+def cos_cx(n)
+  cos(step_angle * n - (step_angle / 2 )) * cr
+end
+
+def sin_cy(n)
+  sin(step_angle * n - (step_angle / 2 )) * cr
 end
 
 def key_pressed
@@ -85,4 +92,3 @@ def key_pressed
     @debug = !debug
   end
 end
-
