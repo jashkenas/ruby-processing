8a5d06c85d66a03ded07df1f5d55f2f4082ed925
monkstone <martin_p@lineon:e.net>
2013-07-01 19:30:47 +0100

    keep in line with vanilla processing
    Former-commit-id: 77c2d83835663275383bb63fc064a5d1a6888943
    Former-commit-id: 882480720473c9e4d1652a6dd54d9ee70b5950b0

--- samples/processing_app/basics/math/sine_cosine.rb
+++ samples/processing_app/basics/math/sine_cosine.rb
@@ -1,55 +1,48 @@
+#
 # Sine Cosine. 
 # 
 # Linear movement with sin() and cos(). 
-# Numbers between 0 and PI*2 (TWO_PI which is roughly 6.28) 
+# Numbers between 0 and PI*2 (TWO_PI which angles roughly 6.28) 
 # are put into these functions and numbers between -1 and 1 are 
-# returned. These values are then scaled to produce larger movements. 
+# returned. These values are then scaled to produce larger movements.
+# not only angle1 angle2 need scope outside draw loop
+#
+ 
+attr_reader :angle1, :angle2
+SCALAR = 70
 
 def setup
-  
-  size 200, 200
-  no_stroke
-  smooth
-  
-  @i = 45
-  @j = 225
-  @pos1 = 0.0
-  @pos2 = 0.0
-  @pos3 = 0.0
-  @pos4 = 0.0
-  @sc = 40
-  
+  size(640, 360)
+  noStroke()
+  rectMode(CENTER)
+  @angle1 = 0
+  @angle2 = 0
 end
 
 def draw
+  background(0)
+
+  ang1 = radians(angle1)
+  ang2 = radians(angle2)
+
+  x1 = width/2 + (SCALAR * cos(ang1))
+  x2 = width/2 + (SCALAR * cos(ang2))
   
-  background 0
+  y1 = height/2 + (SCALAR * sin(ang1))
+  y2 = height/2 + (SCALAR * sin(ang2))
   
-  fill 51
-  rect 60, 60, 80, 80
+  fill(255)
+  rect(width * 0.5, height * 0.5, 140, 140)
+
+  fill(0, 102, 153)
+  ellipse(x1, height * 0.5 - 120, SCALAR, SCALAR)
+  ellipse(x2, height * 0.5 + 120, SCALAR, SCALAR)
   
-  fill 255
-  ellipse @pos1, 36, 32, 32
-  
-  fill 153
-  ellipse 36, @pos2, 32, 32
-  
-  fill 255
-  ellipse @pos3, 164, 32, 32
-  
-  fill 153
-  ellipse 164, @pos4, 32, 32
-  
-  @i += 3
-  @j -= 3
-  
-  ang1 = radians @i
-  ang2 = radians @j
-  
-  @pos1 = width/2 + (@sc * cos( ang1 ))
-  @pos2 = width/2 + (@sc * sin( ang1 ))
-  @pos3 = width/2 + (@sc * cos( ang2 ))
-  @pos4 = width/2 + (@sc * sin( ang2 ))
-  
+  fill(255, 204, 0)
+  ellipse(width * 0.5 - 120, y1, SCALAR, SCALAR)
+  ellipse(width * 0.5 + 120, y2, SCALAR, SCALAR)
+
+  @angle1 += 2
+  @angle2 += 3  
 end
 
