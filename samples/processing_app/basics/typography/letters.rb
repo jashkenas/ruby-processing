9bcaed2170acc37f374a4c6e4cde310a921d7d7b
monkstone <martin_p@lineon:e.net>
2013-07-08 21:52:26 +0100

    update typography examples
    Former-commit-id: c0ab4e0ebf4892a9dd312adc90f88c4ada0da641
    Former-commit-id: 39f480f6f2de4f01106a64b5d3e821b95fa4c86c

--- samples/processing_app/basics/typography/letters.rb
+++ samples/processing_app/basics/typography/letters.rb
@@ -4,23 +4,26 @@
 # setting the font, and then drawing the letters.
 
 def setup    
-  size 200, 200    
-  @font = create_font "CourierNew", 36 
-  text_font @font, 32
-  text_align CENTER    
+  size 640, 360    
+  @font = create_font "Georgia", 24 
+  text_font @font
+  text_align CENTER, CENTER    
 end
 
 def draw    
   background 0  	
   translate 24, 32  	
   x, y = 0.0, 0.0
-  gap = 30  	
-  letters = ("A".."Z").to_a + ("0".."9").to_a # ranges -> arrays -> joined!  	
+  gap = 30  
+  # ranges -> arrays -> joined!  	
+  letters = ("A".."Z").to_a + ("0".."9").to_a  + ("a".."z").to_a
   letters.each do |letter|  	    
     fill 255
     fill 204, 204, 0 if letter =~ /[AEIOU]/
+    fill 0, 204, 204 if letter =~ /[aeiou]/
     fill 153 if letter =~ /[0-9]/
-    fill 255, 100, 0 if key_pressed? && (letter.downcase.eql? key)  		
+    fill 255, 100, 0 if key_pressed? && (letter.downcase.eql? key) 
+ 		fill 0, 100, 255 if key_pressed? && (letter.upcase.eql? key)  	
     text letter, x, y  		
     x += gap  		
     if x > width - 30
