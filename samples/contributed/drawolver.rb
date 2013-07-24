6cad4b456df473d9599ae8b6cdd8297a8a598022
monkstone <martin_p@lineone.net>
2013-07-24 06:20:18 +0100

    further refine drawolver

--- samples/contributed/drawolver.rb
+++ samples/contributed/drawolver.rb
@@ -4,10 +4,26 @@
 # use PVector and the Array is extended to yield one_of_each 
 # pair of pts. See the drawolver library. Also features the use each_cons, 
 # possibly a rare use for this ruby Enumerable method?
-# 2010-03-22 - fjenett
+# 2010-03-22 - fjenett (revised monkstone 2013-07-22)
 
-load_library 'drawolver'
 attr_reader :drawing_mode, :points, :rot_x, :rot_y, :vertices
+
+module ExtendedArray
+  # send one item from each array, expects array to be 2D:
+  # array [[1,2,3], [a,b,c]] sends
+  # [1,a] , [2,b] , [3,c]
+  def one_of_each( &block )
+    i = 0
+    one = self[0]
+    two = self[1]
+    mi = one.length > two.length ? two.length : one.length
+    while i < mi do
+      yield( one[i], two[i] )
+      i = i + 1
+    end
+  end
+end
+
 
 def setup 
   size 640, 360, P3D
@@ -27,19 +43,19 @@ def draw
   end 
   no_fill
   stroke 0  
-  points.each_cons(2) do |ps, pe|   
-    line ps.x, ps.y, pe.x, pe.y
-  end 
+  points.each_cons(2) { |ps, pe| line ps.x, ps.y, pe.x, pe.y}
+
   if (!drawing_mode)    
     no_stroke
     fill 120
     lights    
     vertices.each_cons(2) do |r1, r2|     
-      begin_shape(TRIANGLE_STRIP)       
-        [r1,r2].one_of_each do |v1, v2|          
-          vertex v1.x, v1.y, v1.z
-          vertex v2.x, v2.y, v2.z
-        end
+		  begin_shape(TRIANGLE_STRIP)
+			ext_array = [r1,r2].extend ExtendedArray # extend an instance of Array
+			ext_array.one_of_each do |v1, v2|          
+				vertex v1.x, v1.y, v1.z
+				vertex v2.x, v2.y, v2.z
+			end
       end_shape
     end
   end 
@@ -54,15 +70,15 @@ end
 
 def mouse_pressed
   reset_scene
-  points.push(RPVector.new(mouse_x, mouse_y))
+  points << RPVector.new(mouse_x, mouse_y)
 end
 
 def mouse_dragged
-  points.push(RPVector.new(mouse_x, mouse_y))
+  points << RPVector.new(mouse_x, mouse_y)
 end
 
 def mouse_released
-  points.push(RPVector.new(mouse_x, mouse_y))
+  points << RPVector.new(mouse_x, mouse_y)
   recalculate_shape
 end
 
@@ -88,5 +104,32 @@ def recalculate_shape
   end
   @drawing_mode = false
 end
+
+# a wrapper around PVector that implements operators methods for +, -, *, /
+#
+class RPVector < Java::ProcessingCore::PVector
+
+  def + ( other )
+    v = RPVector.add( self, other )
+    RPVector.new v.x, v.y, v.z
+  end
+
+  def - ( other )
+    v = RPVector.sub( self, other )
+    RPVector.new v.x, v.y, v.z
+  end
+
+  def * ( other )
+    v = RPVector.mult( self, other )
+    RPVector.new v.x, v.y, v.z
+  end
+
+  def / ( other )
+    v = RPVector.div( self, other )
+    RPVector.new v.x, v.y, v.z
+  end
+
+end
+
 
 
