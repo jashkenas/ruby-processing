84d0009ad12ef00064aa9492e853120c0660c28e
monkstone <martin_p@lineon:e.net>
2013-08-10 09:58:36 +0100

    use esfera sketch to demonstrateuse of signature specific alias

--- samples/processing_app/demo/esfera.rb
+++ samples/processing_app/demo/esfera.rb
@@ -11,6 +11,12 @@ QUANTITY = 16000
 
 attr_reader :orb, :phi, :radius, :rx, :ry
 
+# signature-specific aliases for overloaded methods
+java_alias :background_int, :background, [Java::int]
+java_alias :fill_int, :fill, [Java::int]
+java_alias :stroke_int, :stroke, [Java::int]
+java_alias :stroke_float_float, :stroke, [Java::float, Java::float]
+
 def setup
   size(800, 600, P3D)
   @rx = 0
@@ -24,7 +30,7 @@ def setup
 end
 
 def draw
-  background(0)
+  background_int(0)
   translate(width/2,height/2)
   rxp = ((mouse_x - (width/2))*0.005)
   ryp = ((mouse_y - (height/2))*0.005)
@@ -32,7 +38,7 @@ def draw
   @ry = (ry*0.9)+(ryp*0.1)
   rotate_y(rx)
   rotate_x(ry)
-  fill(0)
+  fill_int 0
   no_stroke
   sphere(radius)
   orb.render self, radius
@@ -42,48 +48,48 @@ def draw
 end
 
 class HairyOrb < Array
-	include Processing::Proxy
-	
-	def render app, radius
-		@app = app
-		each do |hair|
-			off = (app.noise(app.millis() * 0.0005, sin(hair.phi)) - 0.5) * 0.3
-			offb = (app.noise(app.millis() * 0.0007, sin(hair.z) * 0.01) - 0.5) * 0.3
-			thetaff = hair.theta + off
-			phff = hair.phi + offb
-			x = radius * cos(hair.theta) * cos(hair.phi)
-			y = radius * cos(hair.theta) * sin(hair.phi)
-			za = radius * sin(hair.theta)
-			xo = radius * cos(thetaff) * cos(phff)
-			yo = radius * cos(thetaff) * sin(phff)
-			zo = radius * sin(thetaff)
-			xb = xo * hair.len
-			yb = yo * hair.len
-			zb = zo * hair.len
-			app.stroke_weight(1)
-			app.begin_shape(LINES)
-			app.stroke(0)
-			app.vertex(x, y, za)
-			app.stroke(200, 150)
-			app.vertex(xb, yb, zb)
-			app.end_shape()
-		end	
-	end
+  include Processing::Proxy
+  
+  def render app, radius
+    @app = app
+    each do |hair|
+      off = (app.noise(app.millis() * 0.0005, sin(hair.phi)) - 0.5) * 0.3
+      offb = (app.noise(app.millis() * 0.0007, sin(hair.z) * 0.01) - 0.5) * 0.3
+      thetaff = hair.theta + off
+      phff = hair.phi + offb
+      x = radius * cos(hair.theta) * cos(hair.phi)
+      y = radius * cos(hair.theta) * sin(hair.phi)
+      za = radius * sin(hair.theta)
+      xo = radius * cos(thetaff) * cos(phff)
+      yo = radius * cos(thetaff) * sin(phff)
+      zo = radius * sin(thetaff)
+      xb = xo * hair.len
+      yb = yo * hair.len
+      zb = zo * hair.len
+      app.stroke_weight(1)
+      app.begin_shape(LINES)
+      app.stroke_int(0)
+      app.vertex(x, y, za)
+      app.stroke_float_float(200, 150)
+      app.vertex(xb, yb, zb)
+      app.end_shape()
+    end 
+  end
 end
 
-class HairFactory	
-	def self.create_hair radius
-		z = rand(-radius .. radius)
-		phi = rand * TWO_PI
-		len = rand(1.15 .. 1.2)
-		theta = Math.asin(z / radius)
-		return Hair.new(z, phi, len, theta)
-	end	
+class HairFactory 
+  def self.create_hair radius
+    z = rand(-radius .. radius)
+    phi = rand * TWO_PI
+    len = rand(1.15 .. 1.2)
+    theta = Math.asin(z / radius)
+    return Hair.new(z, phi, len, theta)
+  end 
 end
 
 class Hair
-	attr_reader :z, :phi, :len, :theta	
-	def initialize z, phi, len, theta	
-		@z, @phi, @len, @theta	 = z, phi, len, theta	
-	end
+  attr_reader :z, :phi, :len, :theta  
+  def initialize z, phi, len, theta 
+    @z, @phi, @len, @theta = z, phi, len, theta 
+  end
 end
