d13e21743d5a69661460519390e2728abd132e31
monkstone <martin_p@lineone.net>
2013-09-21 21:41:49 +0100

    change extension from yaml to yml, add struct example

--- samples/processing_app/topics/advanced_data/load_save_struct_yaml.rb
+++ samples/processing_app/topics/advanced_data/load_save_struct_yaml.rb
@@ -1,12 +1,13 @@
 ######################################
 # Yet another examples of reading and
 # writing to some form of markup,
-# appropriatetly yaml.
+# appropriatetly yaml using ruby structs 
 # by Martin Prout after Dan Shiffman
-# ###################################
+####################################
 load_library :bubble
 
 attr_reader :bubbles, :bubble_data
+
 
 def setup()
   size(640, 360)
@@ -23,20 +24,14 @@ def draw
 end
 
 def load_data
-  yaml = Psych.load_file("data/data.yml")
-  # parse the source string
-  @bubble_data = BubbleData.new "bubbles"
-
-  # get the bubble_data from the top level hash
-  data = bubble_data.extract_data yaml
+  yaml = Psych.load_file("data/struct_data.yml")
+  # we are storing the data as an array of RubyStruct, in a hash with
+  # a symbol as the key (the latter only to show we can, it makes no sense)
+  data = yaml[:bubbles] 
   @bubbles = []
-  # iterate the bubble_data array, and create an array of bubbles
-  data.each do |point|
-    bubbles << Bubble.new(
-      point["position"]["x"],
-      point["position"]["y"],
-      point["diameter"],
-      point["label"])
+  # iterate the bubble_data array, and populate the array of bubbles
+  data.each do |pt|
+    bubbles << Bubble.new(pt.x, pt.y, pt.diameter, pt.label)
   end
 end
 
@@ -44,36 +39,38 @@ def save_data
   # demonstrate how easy it is to create yaml object from a hash in ruby
   yaml = bubble_data.to_hash.to_yaml
   # overwite existing 'data.yaml' 
-  open("data/data.yml", 'w') {|f| f.write(yaml) }
+  open("data/struct_data.yml", 'w') {|f| f.write(yaml) }
 end
 
 def mouse_pressed
   # create a new bubble instance, where mouse was clicked
+  @bubble_data = BubbleData.new bubbles
   @bubble_data.add_bubble(Bubble.new(mouse_x, mouse_y, rand(40 .. 80), "new label"))
   save_data
   # reload the yaml data from the freshly created file
   load_data
 end
 
+
 class BubbleData
-  attr_reader :name, :data
-  def initialize name = "bubbles"
-    @name = name
-    @data = []
+  attr_reader :data
+  def initialize data
+    @data = data
   end
 
   def add_bubble bubble
     data << bubble
+    if (data.size > 10)
+    # Delete the oldest bubble
+      data.shift
+    end
   end
 
-  def extract_data yaml
-    @data = yaml[name]
-  end
-
+  # Using symbol as hash key for a change.  Thus demonstrating only to show we can store 
+  # more complex data strucures quite readily.
   def to_hash
-    {name => data.map{|point| point.to_hash}}
+    {bubbles: data.map{|point| point.to_struct}}
   end
 
 end
-
 
