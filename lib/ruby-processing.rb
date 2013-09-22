bdba5049f4d06fd8cf1cd732024925337eede52e
Martin Prout <martin_p@lineone.net>
2013-09-22 18:45:46 +0100

    Update ruby-processing.rb

--- lib/ruby-processing.rb
+++ lib/ruby-processing.rb
@@ -14,7 +14,7 @@ require 'ruby-processing/helpers/numeric'
 
 # The top-level namespace, a home for all Ruby-Processing classes.
 module Processing
-  VERSION = "2.1.5" unless defined? Processing::VERSION
+  VERSION = "2.1.6" unless defined? Processing::VERSION
   # Autoload a number of constants that we may end up using.
   autoload :App,                  'ruby-processing/app'
   autoload :Runner,               'ruby-processing/runner'
