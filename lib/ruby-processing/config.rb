470eec47c2b3d33f76bf012f292fd54ecfeac66f
monkstone <martin_p@lineone.net>
2013-10-03 16:44:04 +0100

    use psych to load config

--- lib/ruby-processing/config.rb
+++ lib/ruby-processing/config.rb
@@ -1,6 +1,6 @@
-require "yaml"
+require "psych"
 
 module Processing
   CONFIG_FILE_PATH = File.expand_path("~/.rp5rc")
-  CONFIG = (YAML::load_file(CONFIG_FILE_PATH) rescue {}).freeze 
+  CONFIG = (Psych.load_file(CONFIG_FILE_PATH) rescue {}).freeze
 end
