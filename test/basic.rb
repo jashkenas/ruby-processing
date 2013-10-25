4c0bc018b9e4a2593d5c4b71293a7939f43fa178
monkstone <martin_p@lineone.net>
2013-10-25 06:55:20 +0100

    traffic light test + BIT option build

--- test/basic.rb
+++ test/basic.rb
@@ -1,9 +1,12 @@
+java_alias :background_int, :background, [Java::int]
+
 def setup
   size(300, 300)
   frame_rate(10)
 end
 
-def draw  
+def draw
+  background_int 0  
   if frame_count == 3
     puts "ok"
     exit
--- test/p3d.rb
+++ test/p3d.rb
@@ -1,9 +1,12 @@
+java_alias :background_float_float_float, :background, [Java::float, Java::float, Java::float]
+
 def setup
   size(300, 300, P3D)
   frame_rate(10)
 end
 
-def draw  
+def draw 
+  background_float_float_float 39, 232, 51 
   if frame_count == 3
     puts "ok"
     exit
