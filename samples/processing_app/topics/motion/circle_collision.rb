971975aaa978a47bc930410b48cd8e7b4d57a5bc
monkstone <martin_p@lineone.net>
2014-03-26 14:19:43 +0000

    refine more complicated circle_collision sketch

--- samples/processing_app/topics/motion/circle_collision.rb
+++ samples/processing_app/topics/motion/circle_collision.rb
@@ -1,184 +1,141 @@
 # Based on http://processing.org/learning/topics/circlecollision.html
-# by Joe Holt
+
+load_library :vecmath
 
 
-
-# This inner class demonstrates the use of Ruby-Processing's emulation of
-# Java inner classes. The Balls are able to call Processing::App methods.
 class Ball
-  attr_accessor :x, :y, :r, :m, :vec
-  def initialize(r = 0.0, vec = nil, x = 0.0, y = 0.0)
-    @x, @y, @r = x, y, r
+  attr_accessor :position, :r, :m, :velocity
+  
+  def initialize(x = 0.0, y = 0.0, r = 0.0)
+    @position = Vec2D.new(x, y)
+    @r = r
     @m = r * 0.1
-    @vec = vec
+    @velocity = Vec2D.new(rand(-3.0 .. 3), rand(-3.0 .. 3))
+  end 
+  
+  def update
+    @position += velocity
   end
   
-  def move
-    @x += @vec.x
-    @y += @vec.y
+  def check_boundary width, height 
+    if (position.x > width - r) 
+      position.x = width - r
+      velocity.x *= -1
+    elsif (position.x < r) 
+      position.x = r
+      velocity.x *= -1
+    elsif (position.y > height - r) 
+      position.y = height - r
+      velocity.y *= -1
+    elsif (position.y < r) 
+      position.y = r
+      velocity.y *= -1
+    end    
   end
   
-  def draw
-    r = @r * 2
-    ellipse @x, @y, r, r
-    @px, @py = @x, @y
+  def check_collision balls    
+    balls.each do |other_ball|
+      if !(other_ball.equal?(self))
+        # get distances between the balls components
+        difference = other_ball.position - position
+        
+        # calculate magnitude of the vector separating the balls
+        
+        dist_mag_squared = difference.mag_squared
+        
+        if dist_mag_squared < (r + other_ball.r) * (r + other_ball.r)
+          # get angle of difference
+          theta  = difference.heading
+          # precalculate trig values
+          sine = sin(theta)
+          cosine = cos(theta)
+          
+          # ball_array will hold rotated ball positions. You just
+          # need to worry about ball_array[1] position
+          ball_array = [Ball.new, Ball.new]
+          # other_ball's position is relative to ball's
+          # so you can use the vector between them (difference) as the 
+          # reference point in the rotation expressions.
+          # ball_array[0].x and ball_array[0].y will initialize
+          # automatically to 0.0, which is what you want
+          # since other_ball will rotate around ball
+          ball_array[1].position.x  = cosine * difference.x + sine * difference.y
+          ball_array[1].position.y  = cosine * difference.y - sine * difference.x
+          
+          # rotate Temporary velocities
+          velocity_array = [Vec2D.new, Vec2D.new]
+          velocity_array[0].x  = cosine * velocity.x + sine * velocity.y
+          velocity_array[0].y  = cosine * velocity.y - sine * velocity.x
+          velocity_array[1].x  = cosine * other_ball.velocity.x + sine * other_ball.velocity.y
+          velocity_array[1].y  = cosine * other_ball.velocity.y - sine * other_ball.velocity.x
+          
+          # Now that velocities are rotated, you can use 1D
+          # conservation of momentum equations to calculate 
+          # the final velocity along the x-axis.
+          final_velocities = [Vec2D.new, Vec2D.new]
+          # final rotated velocity for ball
+          final_velocities[0].x = ((m - other_ball.m) * velocity_array[0].x + 2 * other_ball.m * velocity_array[1].x) / (m + other_ball.m)
+          final_velocities[0].y = velocity_array[0].y
+          # final rotated velocity for ball
+          final_velocities[1].x = ((other_ball.m - m) * velocity_array[1].x + 2 * m * velocity_array[0].x) / (m + other_ball.m)
+          final_velocities[1].y = velocity_array[1].y
+          
+          # hack to avoid clumping
+          ball_array[0].position.x += final_velocities[0].x
+          ball_array[1].position.x += final_velocities[1].x
+          
+          # Rotate ball positions and velocities back
+          # Reverse signs in trig expressions to rotate 
+          # in the opposite direction
+          # rotate balls
+          final_positions = [Vec2D.new, Vec2D.new]
+          final_positions[0].x = cosine * ball_array[0].position.x - sine * ball_array[0].position.y
+          final_positions[0].y = cosine * ball_array[0].position.y + sine * ball_array[0].position.x
+          final_positions[1].x = cosine * ball_array[1].position.x - sine * ball_array[1].position.y
+          final_positions[1].y = cosine * ball_array[1].position.y + sine * ball_array[1].position.x
+          
+          # update balls to screen position
+          other_ball.position = position + final_positions[1]
+          @position += final_positions[0]
+          
+          # update velocities
+          velocity.x = cosine * final_velocities[0].x - sine * final_velocities[0].y
+          velocity.y = cosine * final_velocities[0].y + sine * final_velocities[0].x
+          other_ball.velocity.x = cosine * final_velocities[1].x - sine * final_velocities[1].y
+          other_ball.velocity.y = cosine * final_velocities[1].y + sine * final_velocities[1].x
+        end
+      end
+    end
   end
   
-  def erase
-    r = @r * 2
-    rect @px, @py, r, r
+  def display
+    no_stroke
+    fill(204)
+    ellipse(position.x, position.y, r * 2, r * 2)
   end
+  
 end
 
+attr_reader :balls
 
 def setup
-  size 400, 400
-  no_stroke
-  frame_rate 30
-  rect_mode RADIUS    
-  @balls = []
-  5.times { @balls << Ball.new(10, PVector.new(2.15, -1.35), *empty_space(15)) }
-  2.times { @balls << Ball.new(40, PVector.new(-1.65, 0.42), *empty_space(45)) }    
-  @frame_time = nil
-  @frame_count = 0
+  size 640, 360
+  @balls = [
+        Ball.new(100, 400, 20),
+        Ball.new(100, 100, 20),
+        Ball.new(200, 100, 20),
+        Ball.new(700, 400, 60)
+        ]
 end
 
 
 def draw
-  t = Time.now
-  fps = 1.0 / (t - @frame_time) if @frame_time
-  @frame_time = t
-  @frame_count += 1
-  
-  # erase previous screen
-  if @frame_count == 1
-    background 51
-  else
-    fill 51
-    @balls.each { |ball| ball.erase }
-  end
-  
-  # move the balls 
-  fill 240
-  @balls.each do |ball|
-    ball.move
-    ball.draw
-    check_boundary_collision ball
-  end
-  check_object_collisions
-end
-
-
-def empty_space(r)
-  x = y = nil
-  while !x || !empty_space?(x, y, r) do
-    x = rand(width)
-    y = rand(height)
-  end
-  return x, y
-end
-
-
-def empty_space?(x, y, r)
-  @balls.each do |ball|
-    vx = x - ball.x
-    vy = y - ball.y
-    mag = sqrt(vx * vx + vy * vy)
-    return false if mag < r + ball.r
-  end
-  return true
-end
-
-
-def check_object_collisions
-  
-  (0...(@balls.length)).each do |ia|
-    ((ia+1)...(@balls.length)).each do |ib|
-      
-      ba = @balls[ia]
-      bb = @balls[ib]
-      
-      # get distances between the balls components
-      bVect = PVector.new
-      bVect.x = bb.x - ba.x
-      bVect.y = bb.y - ba.y
-      
-      # calculate magnitude of the vector separating the balls
-      bVectMag = bVect.mag
-      next if bVectMag >= ba.r + bb.r
-      # get angle of bVect
-      theta  = atan2(bVect.y, bVect.x)
-      # precalculate trig values
-      sine = sin(theta)
-      cosine = cos(theta)
-      
-      # bTemp will hold rotated ball positions. You just
-      # need to worry about bTemp[1] position
-      bTemp = [Ball.new, Ball.new]
-      # bb's position is relative to ba's
-      # so you can use the vector between them (bVect) as the 
-      # reference point in the rotation expressions.
-      # bTemp[0].x and bTemp[0].y will initialize
-      # automatically to 0.0, which is what you want
-      # since bb will rotate around ba
-      bTemp[1].x  = cosine * bVect.x + sine * bVect.y
-      bTemp[1].y  = cosine * bVect.y - sine * bVect.x
-      
-      # rotate Temporary velocities
-      vTemp = [PVector.new, PVector.new]
-      vTemp[0].x  = cosine * ba.vec.x + sine * ba.vec.y
-      vTemp[0].y  = cosine * ba.vec.y - sine * ba.vec.x
-      vTemp[1].x  = cosine * bb.vec.x + sine * bb.vec.y
-      vTemp[1].y  = cosine * bb.vec.y - sine * bb.vec.x
-      
-      # Now that velocities are rotated, you can use 1D
-      # conservation of momentum equations to calculate 
-      # the final velocity along the x-axis.
-      vFinal = [PVector.new, PVector.new]
-      # final rotated velocity for ba
-      vFinal[0].x = ((ba.m - bb.m) * vTemp[0].x + 2 * bb.m * vTemp[1].x) / (ba.m + bb.m)
-      vFinal[0].y = vTemp[0].y
-      # final rotated velocity for ba
-      vFinal[1].x = ((bb.m - ba.m) * vTemp[1].x + 2 * ba.m * vTemp[0].x) / (ba.m + bb.m)
-      vFinal[1].y = vTemp[1].y
-      
-      # hack to avoid clumping
-      bTemp[0].x += vFinal[0].x
-      bTemp[1].x += vFinal[1].x
-      
-      # Rotate ball positions and velocities back
-      # Reverse signs in trig expressions to rotate 
-      # in the opposite direction
-      # rotate balls
-      bFinal = [Ball.new, Ball.new]
-      bFinal[0].x = cosine * bTemp[0].x - sine * bTemp[0].y
-      bFinal[0].y = cosine * bTemp[0].y + sine * bTemp[0].x
-      bFinal[1].x = cosine * bTemp[1].x - sine * bTemp[1].y
-      bFinal[1].y = cosine * bTemp[1].y + sine * bTemp[1].x
-      
-      # update balls to screen position
-      bb.x = ba.x + bFinal[1].x
-      bb.y = ba.y + bFinal[1].y
-      ba.x = ba.x + bFinal[0].x
-      ba.y = ba.y + bFinal[0].y
-      
-      # update velocities
-      ba.vec.x = cosine * vFinal[0].x - sine * vFinal[0].y
-      ba.vec.y = cosine * vFinal[0].y + sine * vFinal[0].x
-      bb.vec.x = cosine * vFinal[1].x - sine * vFinal[1].y
-      bb.vec.y = cosine * vFinal[1].y + sine * vFinal[1].x
-    end
-  end
-  
-end
-
-
-def check_boundary_collision(ball)
-  unless (ball.r ... width - ball.r).cover? ball.x 
-    ball.vec.x *= -1
-  end
-  unless (ball.r ... height - ball.r).cover? ball.y 
-    ball.vec.y *= -1
+  background(51)  
+  balls.each do |b| 
+    b.update
+    b.display
+    b.check_boundary width, height
+    b.check_collision(balls)
   end
 end
 
