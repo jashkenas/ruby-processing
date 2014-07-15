c1703cde4fd0499bc0a6865d0a6b6394a7453be0
monkstone <martin_p@lineone.net>
2014-07-15 17:00:18 +0100

    create short forms for renderers

--- library/vecmath/vecmath.rb
+++ library/vecmath/vecmath.rb
@@ -3,3 +3,6 @@ require 'rpextras'
 Java::ProcessingVecmathArcball::ArcballLibrary.new.load(JRuby.runtime, false)
 Java::ProcessingVecmathVec2::Vec2Library.new.load(JRuby.runtime, false)
 Java::ProcessingVecmathVec3::Vec3Library.new.load(JRuby.runtime, false)
+
+AppRender = Java::ProcessingVecmath::AppRender
+ShapeRender = Java::ProcessingVecmath::ShapeRender
