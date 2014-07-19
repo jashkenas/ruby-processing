d738c07624077532320518fe74fdb2ec8522c095
monkstone <martin_p@lineone.net>
2014-07-19 06:43:24 +0100

    create sketch specific look up table, dry that code

--- samples/processing_app/demos/graphics/wiggling.rb
+++ samples/processing_app/demos/graphics/wiggling.rb
@@ -1,21 +1,29 @@
 # Press 'w' to start wiggling, space to restore
 # original positions.
 
-load_library :fastmath
-
 attr_reader :cube, :wiggling
 CUBE_SIZE = 320.0
 CIRCLE_RAD = 100.0
-CIRCLE_RES = 36
+CIRCLE_RES = 40
 NOISE_MAG = 1.0
 
 # signature-specific alias for overloaded method 
 java_alias :background_int, :background, [Java::int]
 
+attr_reader :sine, :cosine
  
 def setup
-  size(1024, 768, P3D);    
-  wiggling = false
+  size(1024, 768, P3D);
+  @sine = []
+  @cosine = []
+  wiggling = false 
+  # Dry up that processing code a tiny bit
+  (0 .. TAU).step(TAU / CIRCLE_RES) do |i|
+    sine << CIRCLE_RAD * sin(i)
+  end
+  (0 .. TAU).step(TAU / CIRCLE_RES) do |i|
+    cosine << CIRCLE_RAD * cos(i)
+  end
   @cube = create_cube
 end
 
@@ -74,9 +82,8 @@ def create_cube
   face.end_contour
   face.begin_contour
   CIRCLE_RES.times do |i|
-    angle = 360 * i / CIRCLE_RES
-    x = CIRCLE_RAD * DegLut.sin(angle)
-    y = CIRCLE_RAD * DegLut.cos(angle)
+    x = sine[i]
+    y = cosine[i]
     z = CUBE_SIZE / 2
     face.vertex(x, y, z)
   end
@@ -97,9 +104,8 @@ def create_cube
   face.end_contour
   face.begin_contour
   CIRCLE_RES.times do |i|
-    angle = 360 * i / CIRCLE_RES
-    x = CIRCLE_RAD * DegLut.sin(angle)
-    y = CIRCLE_RAD * DegLut.cos(angle)
+    x = sine[i]
+    y = cosine[i]
     z = -CUBE_SIZE / 2
     face.vertex(x, y, z)
   end
@@ -120,10 +126,9 @@ def create_cube
   face.end_contour
   face.begin_contour
   CIRCLE_RES.times do |i|
-    angle = 360 * i / CIRCLE_RES
     x = CUBE_SIZE / 2
-    y = CIRCLE_RAD * DegLut.sin(angle)
-    z = CIRCLE_RAD * DegLut.cos(angle)
+    y = sine[i]
+    z = cosine[i]
     face.vertex(x, y, z)
   end
   face.end_contour
@@ -143,10 +148,9 @@ def create_cube
   face.end_contour
   face.begin_contour
   CIRCLE_RES.times do |i|
-    angle = 360 * i / CIRCLE_RES
     x = -CUBE_SIZE / 2
-    y = CIRCLE_RAD * DegLut.sin(angle)
-    z = CIRCLE_RAD * DegLut.cos(angle)
+    y = sine[i]
+    z = cosine[i]
     face.vertex(x, y, z)
   end
   face.end_contour
@@ -166,10 +170,9 @@ def create_cube
   face.end_contour
   face.begin_contour
   CIRCLE_RES.times do |i|
-    angle = 360 * i / CIRCLE_RES
-    x = CIRCLE_RAD * DegLut.sin(angle)
+    x = sine[i]
     y = CUBE_SIZE / 2
-    z = CIRCLE_RAD * DegLut.cos(angle)
+    z = cosine[i]
     face.vertex(x, y, z)
   end
   face.end_contour
@@ -189,10 +192,9 @@ def create_cube
   face.end_contour
   face.begin_contour
   CIRCLE_RES.times do |i|
-    angle = 360 * i / CIRCLE_RES
-    x = CIRCLE_RAD * DegLut.sin(angle)
+    x = sine[i]
     y = -CUBE_SIZE / 2
-    z = CIRCLE_RAD * DegLut.cos(angle)
+    z = cosine[i]
     face.vertex(x, y, z)
   end
   face.end_contour
@@ -211,9 +213,8 @@ def restore_cube
   face.set_vertex(2, CUBE_SIZE / 2, CUBE_SIZE / 2, CUBE_SIZE / 2)
   face.set_vertex(3, -CUBE_SIZE / 2, CUBE_SIZE / 2, CUBE_SIZE / 2)
   CIRCLE_RES.times do |i|
-    angle = 360 * i / CIRCLE_RES
-    x = CIRCLE_RAD * DegLut.sin(angle)
-    y = CIRCLE_RAD * DegLut.cos(angle)
+    x = sine[i]
+    y = cosine[i]
     z = CUBE_SIZE / 2
     face.set_vertex(4 + i, x, y, z)
   end
@@ -225,9 +226,8 @@ def restore_cube
   face.set_vertex(2, -CUBE_SIZE / 2, CUBE_SIZE / 2, -CUBE_SIZE / 2)
   face.set_vertex(3, CUBE_SIZE / 2, CUBE_SIZE / 2, -CUBE_SIZE / 2)
   CIRCLE_RES.times do |i|
-    angle = 360 * i / CIRCLE_RES
-    x = CIRCLE_RAD * sin(angle)
-    y = CIRCLE_RAD * cos(angle)
+    x = sine[i]
+    y = cosine[i]
     z = -CUBE_SIZE / 2
     face.set_vertex(4 + i, x, y, z)
   end
@@ -239,10 +239,9 @@ def restore_cube
   face.set_vertex(2, CUBE_SIZE / 2, CUBE_SIZE / 2, -CUBE_SIZE / 2)
   face.set_vertex(3, CUBE_SIZE / 2, CUBE_SIZE / 2, CUBE_SIZE / 2)
   CIRCLE_RES.times do |i|
-    angle = 360 * i / CIRCLE_RES
     x = CUBE_SIZE / 2
-    y = CIRCLE_RAD * DegLut.sin(angle)
-    z = CIRCLE_RAD * DegLut.cos(angle)
+    y = sine[i]
+    z = cosine[i]
     face.set_vertex(4 + i, x, y, z)
   end
   
@@ -253,10 +252,9 @@ def restore_cube
   face.set_vertex(2, -CUBE_SIZE / 2, CUBE_SIZE / 2, CUBE_SIZE / 2)
   face.set_vertex(3, -CUBE_SIZE / 2, CUBE_SIZE / 2, -CUBE_SIZE / 2)
   CIRCLE_RES.times do |i|
-    angle = 360 * i / CIRCLE_RES
     x = -CUBE_SIZE / 2
-    y = CIRCLE_RAD * DegLut.sin(angle)
-    z = CIRCLE_RAD * DegLut.cos(angle)
+    y = sine[i]
+    z = cosine[i]
     face.set_vertex(4 + i, x, y, z)
   end    
   
@@ -267,10 +265,9 @@ def restore_cube
   face.set_vertex(2, CUBE_SIZE / 2, CUBE_SIZE / 2, -CUBE_SIZE / 2)
   face.set_vertex(3, -CUBE_SIZE / 2, CUBE_SIZE / 2, -CUBE_SIZE / 2)
   CIRCLE_RES.times do |i|
-    angle = 360 * i / CIRCLE_RES
-    x = CIRCLE_RAD * DegLut.sin(angle)
+    x = sine[i]
     y = CUBE_SIZE / 2
-    z = CIRCLE_RAD * DegLut.cos(angle)
+    z = cosine[i]
     face.set_vertex(4 + i, x, y, z)
   end    
   
@@ -281,10 +278,9 @@ def restore_cube
   face.set_vertex(2, -CUBE_SIZE / 2, -CUBE_SIZE / 2, -CUBE_SIZE / 2)
   face.set_vertex(3, CUBE_SIZE / 2, -CUBE_SIZE / 2, -CUBE_SIZE / 2)
   CIRCLE_RES.times do |i|
-    angle = 360 * i / CIRCLE_RES
-    x = CIRCLE_RAD * DegLut.sin(angle)
+    x = sine[i]
     y = -CUBE_SIZE / 2
-    z = CIRCLE_RAD * DegLut.cos(angle)
+    z = cosine[i]
     face.set_vertex(4 + i, x, y, z)
   end
 end
--- spec/deglut_spec.rb
+++ spec/deglut_spec.rb
@@ -5,7 +5,7 @@ Java::ProcessingFastmath::DeglutLibrary.new.load(JRuby.runtime, false)
 EPSILON=1.0e-04
 
 describe "#DegLut.sin(-720 .. 720) test" do
-  (-720 .. 720).step(3) { |deg|
+  (-720 .. 720).step(1) { |deg|
     it "should work #{deg}" do
      sine = DegLut.sin(deg)
      expect(sine).to be_within(EPSILON).of(Math.sin(deg * Math::PI / 180))
@@ -15,7 +15,7 @@ end
 
 
 describe "#DegLut.cos(-720 .. 720) test" do
-  (-720 .. 720).step(3) { |deg|
+  (-720 .. 720).step(1) { |deg|
     it "should work #{deg}" do
      cosine = DegLut.cos(deg)
      expect(cosine).to be_within(EPSILON).of(Math.cos(deg * Math::PI / 180))
