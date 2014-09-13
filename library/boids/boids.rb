c2db7db8426c4082a58bb3f4519441d763c6cd5e
monkstone <martin_p@lineone.net>
2014-09-13 14:06:13 +0100

    re-factor boids library part one

--- library/boids/boids.rb
+++ library/boids/boids.rb
@@ -14,22 +14,22 @@ class Boid
     @perch_time = 0.0
   end
   
-   def cohesion(d = 100.0)
+  def cohesion(d = 100.0)
     # Boids gravitate towards the center of the flock,
     # Which is the averaged position of the rest of the boids.
     cvx, cvy, cvz = 0.0, 0.0, 0.0
-    boids.reject{|bd| bd.equal? self}.each do |boid|
-      cvx, cvy, cvz = cvx+boid.x, cvy+boid.y, cvz+boid.z 
+    boids.reject { |bd| bd.equal? self }.each do |boid|
+      cvx, cvy, cvz = cvx + boid.x, cvy + boid.y, cvz + boid.z
     end
     count = boids.length - 1.0
-    cvx, cvy, cvz = cvx/count, cvy/count, cvz/count
+    cvx, cvy, cvz = cvx / count, cvy / count, cvz / count
     return (cvx - x) / d, (cvy - y) / d, (cvz - z) / d
   end
   
   def separation(radius = 10.0)
     # Boids don't like to cuddle.
     svx, svy, svz = 0.0, 0.0, 0.0
-    boids.reject{|bd| bd.equal? self}.each do |boid|
+    boids.reject { |bd| bd.equal? self }.each do |boid|
       dvx, dvy, dvz = x - boid.x, y - boid.y, z - boid.z
       svx += dvx if dvx.abs < radius
       svy += dvy if dvy.abs < radius
@@ -41,15 +41,15 @@ class Boid
   def alignment(d = 5.0)
     # Boids like to fly at the speed of traffic.
     avx, avy, avz = 0.0, 0.0, 0.0
-    boids.reject{|bd| bd.equal? self}.each do |boid|
-      avx, avy, avz = avx+boid.vx, avy+boid.vy, avz+boid.vz
+    boids.reject { |bd| bd.equal? self }.each do |boid|
+      avx, avy, avz = avx + boid.vx, avy + boid.vy, avz + boid.vz
     end
     count = boids.length - 1.0
     avx, avy, avz = avx / count, avy / count, avz / count
     return (avx - vx) / d, (avy - vy) / d, (avz - vz) / d
   end
   
-  def limit(max=30.0)
+  def limit(max = 30.0)
     # Tweet, Tweet! The boid police will bust you for breaking the speed limit.
     most = [@vx.abs, @vy.abs, @vz.abs].max
     return if most < max
@@ -90,11 +90,11 @@ class Boids
   end
          
   def self.flock(n, x, y, w, h)
-    return Boids.new.setup(n, x, y, w, h)
+    Boids.new.setup(n, x, y, w, h)
   end
   
   def setup(n, x, y, w, h)
-    n.times do |i|
+    n.times do
       dx, dy = rand(w), rand(h)
       z = rand(200.0)
       self << Boid.new(self, x + dx, y + dy, z)
@@ -106,11 +106,11 @@ class Boids
     @scatter_i = 0.0
     @perch = 1.0 # Lower this number to divebomb.
     @perch_y = h
-    @perch_t = -> {rand(25..75.0)}
+    @perch_t = -> { rand(25..75.0) }
     @has_goal = false
     @flee = false
     @goal_x = @goal_y = @goal_z = 0.0
-    return self
+    self
   end
   
   def scatter(chance = 0.005, frames = 50.0)
@@ -123,7 +123,7 @@ class Boids
   end
   
   def perch(ground = nil, chance = 1.0, frames = nil)
-    frames ||= -> {rand(25..75.0)}
+    frames ||= -> { rand(25..75.0) }
     ground ||= h
     @perch, @perch_y, @perch_t = chance, ground, frames
   end
@@ -145,40 +145,36 @@ class Boids
   def constrain
     # Put them boids in a cage.
     dx, dy = w * 0.1, h * 0.1
-    self.each do |b|
+    each do |b|
       b.vx += rand(dx) if b.x < x - dx
       b.vx += rand(dy) if b.y < y - dy
       b.vx -= rand(dx) if b.x > x + w + dx
       b.vy -= rand(dy) if b.y > y + h + dy
       b.vz += 10.0 if b.z < 0.0
       b.vz -= 10.0 if b.z > 100.0
-      
-      if b.y > @perch_y && rand < @perch
-        b.y = @perch_y
-        b.vy = -(b.vy.abs) * 0.2
-        b.is_perching = true
-        @perch_t.respond_to?(:call) ? b.perch_time = @perch_t.call : b.perch_time = @perch_t
-      end
+      next unless b.y > @perch_y && rand < @perch
+      b.y = @perch_y
+      b.vy = -(b.vy.abs) * 0.2
+      b.is_perching = true
+      @perch_t.respond_to?(:call) ? b.perch_time = @perch_t.call : b.perch_time = @perch_t
     end
   end
     
-  def update(opts={}) # Just flutter, little boids ... just flutter away.  	   
+  def update(opts = {}) # Just flutter, little boids ... just flutter away.  	   
     options = {
-        shuffled: true,     #  Shuffling keeps things flowing smooth.
-        cohesion: 100.0, 
-        separation: 10.0, 
-        alignment: 5.0, 
-        goal: 20.0, 
-        limit: 30.0
-    } 
+      shuffled: true,     #  Shuffling keeps things flowing smooth.
+      cohesion: 100.0,
+      separation: 10.0,
+      alignment: 5.0,
+      goal: 20.0,
+      limit: 30.0
+    }
     options.merge! opts
-    
-    self.shuffle! if options[:shuffled]
+    shuffle! if options[:shuffled]
     m1 = 1.0   # cohesion
     m2 = 1.0   # separation
     m3 = 1.0   # alignment
     m4 = 1.0   # goal
-    
     @scattered = true if !(@scattered) && rand < @scatter
     if @scattered
       m1 = -m1
@@ -189,11 +185,9 @@ class Boids
       @scattered = false
       @scatter_i = 0.0
     end
-    
     m4 = 0.0 unless @has_goal
     m4 = -m4 if @flee
-    
-    self.each do |b|
+    each do |b|
       if b.is_perching
         if b.perch_time > 0.0
           b.perch_time -= 1.0
@@ -202,24 +196,18 @@ class Boids
           b.is_perching = false
         end
       end
-      
       vx1, vy1, vz1 = b.cohesion(options[:cohesion])
       vx2, vy2, vz2 = b.separation(options[:separation])
       vx3, vy3, vz3 = b.alignment(options[:alignment])
       vx4, vy4, vz4 = b.goal(@goal_x, @goal_y, @goal_z, options[:goal])
-      
-      b.vx += m1*vx1 + m2*vx2 + m3*vx3 + m4*vx4
-      b.vy += m1*vy1 + m2*vy2 + m3*vy3 + m4*vy4
-      b.vz += m1*vz1 + m2*vz2 + m3*vz3 + m4*vz4
-      
+      b.vx += m1 * vx1 + m2 * vx2 + m3 * vx3 + m4 * vx4
+      b.vy += m1 * vy1 + m2 * vy2 + m3 * vy3 + m4 * vy4
+      b.vz += m1 * vz1 + m2 * vz2 + m3 * vz3 + m4 * vz4
       b.limit(options[:limit])
-  
       b.x += b.vx
       b.y += b.vy
       b.z += b.vz
     end
-    self.constrain
+    constrain
   end
 end
-  
-  
