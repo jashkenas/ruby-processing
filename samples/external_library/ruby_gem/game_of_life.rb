f8b1ca6e980cbf6e46386648e7cd7632ef30f394
monkstone <martin_p@lineone.net>
2014-10-07 19:22:05 +0100

    clean up gof

--- samples/external_library/ruby_gem/game_of_life.rb
+++ samples/external_library/ruby_gem/game_of_life.rb
@@ -39,69 +39,63 @@ end
 
 def draw
   background(0)
-  #Draw live cells
-  (0 ... row).each do |x|
-    (0 ... column).each do |y|
-      if (cells.get([x, y]))
-        fill(alive)
-        rect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
-      end
+  # Draw live cells
+  (0...row).each do |x|
+    (0...column).each do |y|
+      next unless cells.get([x, y])
+      fill(alive)
+      rect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
     end
   end
   # Iterate if timer ticks
-  if (millis - last_time > INTERVAL)
-    if (!pause)
-      tick!
-      @last_time = millis
-    end
-  end
-  
+  unless pause
+    tick!
+    @last_time = millis
+  end if millis - last_time > INTERVAL
   # Create  new cells manually on pause
-  if (pause && mouse_pressed?)
+  if pause && mouse_pressed?
     # # Map and avoid out of bound errors
-    x_cell_over = (map(mouse_x, 0, width, 0, row)).to_i
-    x_cell_over = constrain(x_cell_over, 0, row - 1)
-    y_cell_over = (map(mouse_y, 0, height, 0, column)).to_i
-    y_cell_over = constrain(y_cell_over, 0, column - 1)
-    
+    x_cell_over = (map1d(mouse_x, (0..width), (0..row))).to_i
+    x_cell_over = (0..row - 1).clip(x_cell_over)
+    y_cell_over = (map1d(mouse_y, (0..height), (0..column))).to_i
+    y_cell_over = (0..column - 1).clip(y_cell_over)
     # Check against cells in buffer
-    if (cells_buffer.get([x_cell_over, y_cell_over]))  # Cell is alive
+    if cells_buffer.get([x_cell_over, y_cell_over])  # Cell is alive
       cells.set([x_cell_over, y_cell_over], DEAD) # Kill
-      fill(0) #reflect changed status
+      fill(0) # reflect changed status
     else  # Cell is dead
       cells.set([x_cell_over, y_cell_over], ALIVE) # Make alive
       fill(alive) # Fill alive color
     end
-    
-  elsif (pause && !mouse_pressed?)  # And then save to buffer once mouse goes up
-    # Save cells to buffer (so we operate with one array keeping the other intact)
+  elsif pause && !mouse_pressed?  # And then save to buffer once mouse goes up
+    # Save cells to buffer
+    # (so we operate with one array keeping the other intact)
     @cells_buffer = cells.copy
   end
 end
 
 def tick!  # When the clock ticks
-  # Save cells to buffer (so we operate with one array keeping the other intact)
+  # Save cells to buffer
+  # (so we operate with one array keeping the other intact)
   @cells_buffer = cells.copy
   # Visit each cell:
-  (0 ... row).each do |x|
-    (0 ... column).each do |y|
+  (0...row).each do |x|
+    (0...column).each do |y|
       # And visit all the neighbours of each cell
       neighbours = 0 # We'll count the neighbours
       (x - 1..x + 1).each do |xx|
         (y - 1..y + 1).each do |yy|
           # Make sure you are not out of bounds
-          if [(xx>=0), (xx<row), (yy>=0), (yy<column)].all? {|in_bounds| in_bounds == true}
-            # Make sure to check against self
-            if ![(xx == x), (yy == y)].all? {|is_self| is_self == true}
-              if (cells_buffer.get([xx, yy])) # true == ALIVE
-                neighbours += 1 # Check alive neighbours and count them
-              end # alive
-            end # End of if self
+          next unless [(xx >= 0), (xx < row), (yy >= 0), (yy < column)].all?
+          # Make sure to check against self
+          unless [(xx == x), (yy == y)].all?
+            next unless cells_buffer.get([xx, yy]) # true == ALIVE
+            neighbours += 1 # Check alive neighbours and count them
           end # End of if grid bounds
         end # End of yy loop
-      end #End of xx loop
+      end # End of xx loop
       # We've checked the neighbours: apply rules in one line (only in ruby)!
-      cells.set([x, y], (cells_buffer.get([x, y]))?  ((2..3) === neighbours) : (neighbours == 3))
+      cells.set([x, y], (cells_buffer.get([x, y])) ? ((2..3) === neighbours) : (neighbours == 3))
     end # End of y loop
   end # End of x loop
 end # End of function
@@ -109,7 +103,7 @@ end # End of function
 def key_pressed
   case key
   when 'r', 'R'
-    # Restart: reinitialization of cells 
+    # Restart: reinitialization of cells
     @cells = MDArray.boolean([row, column], random_data)
   when ' ' # On/off of pause
     @pause = !pause
@@ -119,9 +113,5 @@ def key_pressed
 end
 
 def random_data
-  data = []
-  (0 ... row * column).each do
-    data << (rand(1000) < ALIVE_START)
-  end
-  return data
+  (0...row * column).map { rand(1000) < ALIVE_START }
 end
