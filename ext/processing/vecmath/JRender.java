6c9b10101469bb5c3dbf71d5f3580771a31ffbef
monkstone <martin_p@lineone.net>
2014-10-26 10:44:07 +0000

    implement :to_curve_vertex

--- ext/processing/vecmath/JRender.java
+++ ext/processing/vecmath/JRender.java
@@ -6,7 +6,9 @@ package processing.vecmath;
  */
 public interface JRender {  
     public void vertex(double x, double y);
+    public void curveVertex(double x, double y);
     public void vertex(double x, double y, double z);
     public void vertex(double x, double y, double z, double u, double v);
+    public void curveVertex(double x, double y, double z);
     public void normal(double x, double y, double z);
 }
