34352795239673d97ad0a1792dadbc7e7c8589b9
monkstone <martin_p@lineone.net>
2014-11-26 21:09:08 +0000

    replace erb with format

--- lib/ruby-processing/exporters/creator.rb
+++ lib/ruby-processing/exporters/creator.rb
@@ -1,44 +1,79 @@
+BASIC = <<-CODE
+def setup
+  size %s, %s
+end
+
+def draw
+
+end
+CODE
+
+BASIC_MODE = <<-CODE
+def setup
+  size %s, %s, %s
+end
+
+def draw
+
+end
+CODE
+
+CLASS_BASIC = <<-CODE
+class %s
+  def setup
+    size %s, %s
+  end
+  
+  def draw
+  
+  end
+end
+CODE
+
+CLASS_MODE = <<-CODE
+class %s
+  def setup
+    size %s, %s, %s
+  end
+  
+  def draw
+  
+  end
+end
+CODE
+
+INNER = <<-CODE
+class %s
+  include Processing::Proxy
+
+end
+CODE
+
 module Processing
-  require 'erb'
-
+  # Write file to disk
   class SketchWriter
-    include ERB::Util
-
-    def initialize(template, param = {})
-      @name = param[:name]
-      @file_name = param[:file_name]
-      @title = param[:title]
-      @width = param[:width]
-      @height = param[:height]
-      @mode = param[:mode].upcase unless param[:mode].nil?
-      @template = template
-      save(@file_name)
+    def initialize(template, file_name)
+      save(template, file_name)
     end
 
-    def render
-      ERB.new(@template).result(binding)
-    end
+    private
 
-    def save(file)
+    def save(template, file)
       File.open(file, 'w+') do |f|
-        f.write(render)
+        f.write(template)
       end
     end
   end
 
   # An abstract class providing common methods for real creators
   class Creator
-
     ALL_DIGITS = /\A\d+\Z/
 
     def already_exist(path)
       new_file = "#{File.dirname(path)}/#{path.underscore}.rb"
-      if !File.exist?(path) && !File.exist?(new_file)
-        return
-      else
-        puts 'That file already exists!'
-        exit
-      end
+      return if !File.exist?(path) && !File.exist?(new_file)
+      puts 'That file already exists!'
+      exit
     end
 
     # Show the help/usage message for create.
@@ -55,89 +90,40 @@ module Processing
 
       USAGE
     end
-
   end
 
   # This class creates bare sketches, with an optional render mode
   class BasicSketch < Creator
     # Create a blank sketch, given a path.
     def basic_template
-%(
-def setup
-  size <%=@width%>, <%=@height%>
-end
-
-def draw
-
-end
-)
+      format(BASIC, @width, @height)
     end
 
     def basic_template_mode
-%(
-def setup
-  size <%=@width%>, <%=@height%>, <%=@mode%>
-end
-
-def draw
-
-end
-)
+      format(BASIC_MODE, @width, @height, @mode)
     end
 
     def create!(path, args)
       return usage if /\?/ =~ path || /--help/ =~ path
-      main_file = File.basename(path, '.rb')
       # Check to make sure that the main file doesn't exist already
       already_exist(path)
-      @param = {
-        name: main_file.camelize,
-        file_name: "#{File.dirname(path)}/#{path.underscore}.rb",
-        title: main_file.titleize,
-        width: args[0],
-        height: args[1],
-        mode: args[2]
-      }
-      @with_size = @param[:width] && @param[:width].match(ALL_DIGITS) &&
-        @param[:height] && @param[:height].match(ALL_DIGITS)
-      template = @param[:mode].nil? ? basic_template : basic_template_mode
-      SketchWriter.new(template, @param)
+      @file_name = "#{File.dirname(path)}/#{path.underscore}.rb"
+      @width = args[0]
+      @height = args[1]
+      @mode = args[2].upcase unless args[2].nil?
+      template = @mode.nil? ? basic_template : basic_template_mode
+      SketchWriter.new(template, @file_name)
     end
   end
 
   # This class creates class wrapped sketches, with an optional render mode
   class ClassSketch < Creator
-
     def class_template
-%(
-class <%=@name%> < Processing::App
-  def setup
-    size <%=@width%>, <%=@height%>
-  end
-
-  def draw
-
-  end
-end
-
-# <%=@name%>.new(x: 20, y: 20)
-)
+      format(CLASS_BASIC, @name, @width, @height)
     end
 
     def class_template_mode
-%(
-class <%=@name%> < Processing::App
-  def setup
-    size <%=@width%>, <%=@height%>, <%=@mode%>
-  end
-
-  def draw
-
-  end
-end
-
-# <%=@name%>.new(x: 20, y: 20)
-)
+      format(CLASS_MODE, @name, @width, @height, @mode)
     end
     # Create a bare blank sketch, given a path.
     def create!(path, args)
@@ -145,30 +131,21 @@ end
       main_file = File.basename(path, '.rb')
       # Check to make sure that the main file doesn't exist already
       already_exist(path)
-      @param = {
-        name: main_file.camelize,
-        file_name: "#{File.dirname(path)}/#{path.underscore}.rb",
-        title: main_file.titleize,
-        width: args[0],
-        height: args[1],
-        mode: args[2]
-      }
-      @with_size = @param[:width] && @param[:width].match(ALL_DIGITS) &&
-        @param[:height] && @param[:height].match(ALL_DIGITS)
-      template = @param[:mode].nil? ? class_template : class_template_mode
-      SketchWriter.new(template, @param)
+      @name = main_file.camelize
+      @file_name = "#{File.dirname(path)}/#{path.underscore}.rb"
+      @title = main_file.titleize
+      @width = args[0]
+      @height = args[1]
+      @mode = args[2].upcase unless args[2].nil?
+      template = @mode.nil? ? class_template : class_template_mode
+      SketchWriter.new(template, @file_name)
     end
   end
 
   # This class creates a ruby-processing class that mimics java inner class
   class Inner < Creator
     def inner_class_template
-%(
-class <%=@name%>
-  include Processing::Proxy
-
-end
-)
+      format(INNER, @name)
     end
     # Create a blank sketch, given a path.
     def create!(path, _args_)
@@ -176,12 +153,9 @@ end
       main_file = File.basename(path, '.rb')
       # Check to make sure that the main file doesn't exist already
       already_exist(path)
-      @param = {
-        name: main_file.camelize,
-        file_name: "#{File.dirname(path)}/#{path.underscore}.rb",
-        title: main_file.titleize
-      }
-      SketchWriter.new(inner_class_template, @param)
+      @name = main_file.camelize
+      @file_name = "#{File.dirname(path)}/#{path.underscore}.rb"
+      SketchWriter.new(inner_class_template, @file_name)
     end
   end
 end
