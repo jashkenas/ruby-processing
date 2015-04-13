a9ff76024e4f03e70ae3bc4971b667d9b5c546d6
monkstone <martin_p@lineone.net>
2015-04-13 16:21:10 +0100

    add constrained_map

--- spec/helper_methods_spec.rb
+++ spec/helper_methods_spec.rb
@@ -1,10 +1,39 @@
 require_relative '../lib/ruby-processing/helper_methods'
-require_relative '../lib/ruby-processing/helpers/range.rb'
 
 include Processing::HelperMethods
 
 EPSILON = 1.0e-04
 
+describe 'constrained_map included' do
+  it 'should return constrained_map(x, range1, range2)' do
+    x = 0
+    range1 = (0..10)
+    range2 = (100..1)
+    expect(constrained_map(x, range1, range2)).to eq 100
+  end
+end
+
+describe 'constrained_map reverse' do
+  it 'should return constrained_map(x, range1, range2)' do
+    x0 = 10.1
+    x1 =  -2
+    range1 = (0..10)
+    range2 = (100..1)
+    expect(constrained_map(x0, range1, range2)).to eq 1
+    expect(constrained_map(x1, range1, range2)).to eq 100
+  end
+end
+
+describe 'constrained_map forward' do
+  it 'should return constrained_map(x, range1, range2)' do
+    x0 = 10.1
+    x1 =  -2
+    range1 = (0..10)
+    range2 = (1..100)
+    expect(constrained_map(x0, range1, range2)).to eq 100
+    expect(constrained_map(x1, range1, range2)).to eq 1
+  end
+end
 
 describe '2D#dist' do
   it 'should return dist(ax, ay, ab, ay)' do
@@ -73,47 +102,5 @@ describe '3D#dist' do
   it 'should return dist(ax, ay, az, bx, by, bz)' do
     ax, ay, bx, by, cx, cy = 0, 0, 1.0, 1.0, 0, 0
     expect(dist(ax, ay, bx, by, cx, cy)).to eq Math.sqrt(2)
-  end
-end
-
-describe 'Range#clip_lo' do
-  it 'should return (a..b).clip(x)' do
-    a, b, x0 = 0, 100, -10
-    expect((a..b).clip(x0)).to eq a  
-  end
-end
-
-describe 'Range#clip_hi' do
-  it 'should return (a..b).clip(x1)' do
-    a, b, x1 = 0, 100, 200
-    expect((a..b).clip(x1)).to eq b
-  end
-end
-
-describe 'Range#clip_val' do
-  it 'should return (a..b).clip(x2)' do
-    a, b, x2 = 0, 100, 50
-    expect((a..b).clip(x2)).to eq x2
-  end
-end
-
-describe 'constrained_map#lo' do
-  it 'should return constrained_map' do
-    r1, r2, x0 = (0..100), (20..200), -10
-    expect(constrained_map(x0, r1, r2)).to eq 20  
-  end
-end
-
-describe 'constrained_map#hi' do
-  it 'should return constrained_map' do
-    r1, r2, x1 = (0..100), (200..20), 200
-    expect(constrained_map(x1, r1, r2)).to eq 20 
-  end
-end
-
-describe 'constrained_map#val' do
-  it 'should return constrained_map' do
-    r1, r2, x1 = (0..100), (20..200), 50
-    expect(constrained_map(x1, r1, r2)).to eq 110.0
   end
 end
